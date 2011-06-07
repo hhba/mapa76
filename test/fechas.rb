5ebe3cbaa81b3dc9a941c0ccd41587cde0033131
Martin Sarsale <martin@sumavisos.com>
2011-06-07 00:18:36 -0300

    mas formatos de fecha

--- test/fechas.rb
+++ test/fechas.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require "./lib/procesar_texto"
 require "test/unit"
  
@@ -12,6 +13,9 @@ class TestFechas < Test::Unit::TestCase
     d << "diciembre de 1978"
     d << "3/11"
     d << "27/11/77"
+    d << "abril del ´78"
+    d << "fines de noviembre 1977"
+    
     t=ProcesarTexto.new(d)
     f=t.fechas
     assert_equal(Date.civil(1977,12,3),  f[0] )
@@ -19,6 +23,8 @@ class TestFechas < Test::Unit::TestCase
     assert_equal(Date.civil(1978,7),     f[2] ) 
     assert_equal(Date.civil(0,12,6),     f[3] )
     assert_equal(Date.civil(1978,12,1),  f[4] )
-    assert_equal(Date.civil(1977,11,27),  f[5] )
+    assert_equal(Date.civil(1977,11,27), f[5] )
+    assert_equal(Date.civil(0,4,28),     f[6] )
+    assert_equal(Date.civil(1977,11,1),  f[7] )
   end
 end
