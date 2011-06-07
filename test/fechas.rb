a94559082c8d487f846de755ac01faadaf665640
Martin Sarsale <martin@sumavisos.com>
2011-06-06 23:37:05 -0300

    mas formatos de fecha

--- test/fechas.rb
+++ test/fechas.rb
@@ -10,6 +10,8 @@ class TestFechas < Test::Unit::TestCase
     d << "julio del 78"
     d << "6 de diciembre"
     d << "diciembre de 1978"
+    d << "3/11"
+    d << "27/11/77"
     t=ProcesarTexto.new(d)
     f=t.fechas
     assert_equal(Date.civil(1977,12,3),  f[0] )
@@ -17,6 +19,6 @@ class TestFechas < Test::Unit::TestCase
     assert_equal(Date.civil(1978,7),     f[2] ) 
     assert_equal(Date.civil(0,12,6),     f[3] )
     assert_equal(Date.civil(1978,12,1),  f[4] )
+    assert_equal(Date.civil(1977,11,27),  f[5] )
   end
-
 end
