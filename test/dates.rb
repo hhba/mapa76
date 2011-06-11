2e76dc4669bb1042e62bf7097a550fec1da2a50d
Martin Sarsale <martin@sumavisos.com>
2011-06-11 12:58:34 -0300

    date bugfix

--- test/dates.rb
+++ test/dates.rb
@@ -5,26 +5,34 @@ require "test/unit"
 class TestDates < Test::Unit::TestCase
    
   def test_simple
-    d = []
-    d << " 3 de diciembre del 77."  
-    d << "3 de diciembre del 1977."
-    d << "julio del 78"
-    d << "6 de diciembre"
-    d << "diciembre de 1978"
-    d << "3/11"
-    d << "27/11/77"
-    d << "abril del ´78"
-    d << "fines de noviembre 1977"
+    t=Text.new " 3 de diciembre del 77."  
+    assert_equal(Date.civil(1977,12,3).to_s  ,  t.dates.first.to_s )
     
-    t=Text.new(d)
-    f=t.dates
-    assert_equal(Date.civil(1977,12,3),  f[0] )
-    assert_equal(Date.civil(1977,12,3),  f[1] )
-    assert_equal(Date.civil(1978,7),     f[2] ) 
-    assert_equal(Date.civil(0,12,6),     f[3] )
-    assert_equal(Date.civil(1978,12,1),  f[4] )
-    assert_equal(Date.civil(1977,11,27), f[5] )
-    assert_equal(Date.civil(0,4,28),     f[6] )
-    assert_equal(Date.civil(1977,11,1),  f[7] )
+    t=Text.new "3 de diciembre del 1977."
+    assert_equal(Date.civil(1977,12,3).to_s,  t.dates.first.to_s )
+    
+    t=Text.new "julio del 78"
+    assert_equal(Date.civil(1978,7).to_s,     t.dates.first.to_s ) 
+
+    t=Text.new "6 de diciembre"
+    assert_equal(Date.civil(0,12,6).to_s,     t.dates.first.to_s )
+
+    t=Text.new "diciembre de 1978"
+    assert_equal(Date.civil(1978,12,1).to_s,  t.dates.first.to_s )
+
+    t=Text.new "3/11"
+
+    t=Text.new "27/11/77"
+    assert_equal(Date.civil(1977,11,27).to_s, t.dates.first.to_s )
+
+    t=Text.new "abril del ´78"
+    assert_equal(Date.civil(1978,4,1).to_s,     t.dates.first.to_s )
+
+    t=Text.new "fines de noviembre 1977"
+    assert_equal(Date.civil(1977,11,1).to_s,  t.dates.first.to_s )
+
+    t=Text.new "juicio del 25 de junio de 2008. "
+    assert_equal(Date.civil(2008,6,25).to_s,  t.dates.first.to_s )
+    
   end
 end
