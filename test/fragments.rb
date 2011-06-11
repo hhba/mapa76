64e95c377825096f959dca4cd39071d374e21b71
Martin Sarsale <martin@sumavisos.com>
2011-06-11 16:55:23 -0300

    cache buster for test

--- test/fragments.rb
+++ test/fragments.rb
@@ -1,10 +1,14 @@
 #encoding: utf-8
 require File.join(File.expand_path(File.dirname(__FILE__)),"/../lib/text")
 require "test/unit"
- 
+module Cacheable
+  def cache_load(foo)
+    :notcached
+  end
+end
 class TestFragments < Test::Unit::TestCase
   def test_fragment_id
-    str = StringDocument.new("aa Pedro Gomez ááá Róqúé Péréz")
+    str = StringDocument.new("aa Pedro Gomez ááá Róqúé Péréz #{"a " * 500} Don Nadie")
     str.id = 33
     doc = Text.new(str)
     names = doc.person_names
@@ -15,8 +19,10 @@ class TestFragments < Test::Unit::TestCase
     assert_equal( "frag:doc=33:22-38", roque.id ) 
     assert_equal( "Róqúé Péréz", roque.context(0))
 
-    pedro_context = pedro.extract
+    pedro_context = pedro.context.extract
     pedro_context_names = pedro_context.person_names
+
+    assert_equal( 2 , pedro_context_names.length ) 
     
     roque_from_pedro_context = pedro_context_names[1]
     assert_equal( "frag:doc=33:22-38", roque_from_pedro_context.id ) 
