dce02eca7c1880d573e178c411b3773a88ede2c9
Martin Sarsale <martin@sumavisos.com>
2011-06-18 12:02:02 -0300

    person name tests

--- test/person_names.rb
+++ test/person_names.rb
@@ -8,6 +8,12 @@ class TestPersonNames < Test::Unit::TestCase
     pt=Text.new(d)
     n=pt.person_names
     assert_equal("Turco Julian",n[0])
+    pt=Text.new("onectaron con empresarios del nivel de Roberto Rocca, Enrique Piñeiroi, Franco Macri, toda gente conocida de su padre que habia ")
+    n=pt.person_names
+    assert_equal("Roberto Rocca", n[0])
+    assert_equal("Enrique Piñeiroi", n[1])
+    assert_equal("Franco Macri", n[2])
+
   end
 end
 
