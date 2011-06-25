9251f7c14d9996e39b95fd15213f9c0946ef1662
Martin Sarsale <martin@sumavisos.com>
2011-06-25 13:37:01 -0300

    incomplete dates

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,3 +1,12 @@
 class Milestone < Sequel::Model
   many_to_one :person
+  def date_range(d)
+    IncompleteDate.parse(d)
+  end
+  def date_from_range
+    date_range(date_from)
+  end
+  def date_to_range
+    date_range(date_to)
+  end
 end
