9bdd90ded639de4df725aab93a42249abbbd6fc5
Martin Sarsale <martin@sumavisos.com>
2011-08-02 00:31:01 -0300

    refactored controller hierarchy

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -1,14 +1,14 @@
 require "json"
-Alegato.controllers do
+Alegato.controllers :admin do
   layout :admin
-  get "/admin" do
+  get :index do
     @docs = Document.all
     render "admin/doc_list", 
   end
-  get :doc_import, "/admin/import" do
+  get :import do
     render "admin/import"
   end
-  put :doc_import,"/admin/import" do
+  put :import do
     if params[:text]
       d = Document.new
       d.title = params[:title]
@@ -20,58 +20,16 @@ Alegato.controllers do
       end
     end
   end
-  get :doc, :map => "/admin/:id" do
-    @doc = Document[params[:id]]
-    @most_mentioned = @doc.person_dataset.order_by(:mentions).reverse.limit(10)
-    render "admin/doc"
-  end
-  post :reparse_doc, :map => "/admin/:id/reparse" do
-    @doc = Document[params[:id]]
-    @person_names = Hash.new{|hash,key| hash[key]=[]}
-    @doc.extract.person_names.each{|nombre| 
-      @person_names[Person.normalize_name(nombre)] << nombre 
-    }
-    params[:people].each{|n|
-      person_name = Person.normalize_name(n)
-      p = Person.find_or_create(:name => person_name)
-      @doc.add_person(p,@person_names[person_name].length) # link person and document and update the number of times this person gets mentioned
-    }
-  end
-  get :reparse_doc, :map => "/admin/:id/reparse" do
-    @doc = Document[params[:id]]
-    @person_names = Hash.new{|hash,key| hash[key]=[]}
-    @doc.extract.person_names.each{|nombre| 
-      @person_names[Person.normalize_name(nombre)] << nombre 
-    }
-    render "admin/reparse"
-  end
-  get :persons, :map => "/admin/:id/nombres" do
-    @doc = Document[params[:id]]
-    @people = @doc.person
-    render "admin/people_in_doc"
-  end
-  get :person, :with => [:name], :map => "/admin/person/" do
-    @name = params[:name]
-    @persons = Person.filter_by_name(params[:name]).all
+  get :person, :with => [:id] do
+    if params[:id].to_i == 0
+      @persons = Person.filter_by_name(params[:id]).all
+    else
+      @persons = [Person[params[:id]]]
+    end
     render "admin/person"
   end
-  get :nombre, :map => "/admin/:doc_id/nombres/:id" do
-    if params[:id].to_i == 0
-      @person = Person.filter_by_name(params[:id]).first
-    else
-      @person = Person[params[:id]]
-    end
-    @what = Milestone.what_list
-    @where = Milestone.where_list 
-    doc = Document[params[:doc_id]]
-    person_name = ActiveSupport::Inflector.transliterate(@person.name).downcase
-    @fragments = doc.extract.person_names.find_all{|name| 
-      name = ActiveSupport::Inflector.transliterate(name.to_s.downcase)
-      name == person_name
-    }
-    render "admin/person_in_doc"
-  end
-  get :context do
+
+  get :context, :with => :fragment_id do
     if params[:fragment_id]
       data=params[:fragment_id].match(/frag:doc=([0-9]+):([0-9]+)-([0-9]+)/)
       doc_id=data[1]
@@ -91,7 +49,7 @@ Alegato.controllers do
     r={:fragment_id => fragment.fragment_id, :text => markup_fragment(fragment)}.to_json
     r
   end
-  post :classify_name, :map => "/admin/name/classify" do
+  post :classify_name do
     r=false
     if params[:name] and params[:training]
       Text::PersonName.train(params[:training],params[:name])
@@ -99,28 +57,4 @@ Alegato.controllers do
     end
     r.to_json
   end
-  get :person, :map => "/admin/person/" do
-    puts params
-    person = Person.where(:name => params[:name].strip).first || Person.create(:name => params[:name].strip)
-    person.values.to_json
-  end
-  post :person, :map => "/admin/person/" do
-    person = Person.where(:name => params[:name].strip).first || Person.create(:name => params[:name].strip)
-    Array(params[:person][:milestones]).each{|milestone|
-      data = milestone.dup
-      data.delete("id")
-      data[:what] = ! data["what_txt"].blank? ? data["what_txt"] : data["what_opc"]
-      data.delete("what_txt")
-      data.delete("what_opc")
-      data[:where] = ! data["where_txt"].blank? ? data["where_txt"] : data["where_opc"]
-      data.delete("where_txt")
-      data.delete("where_opc")
-      puts data.inspect
-      m=Milestone.new(data)
-      person.add_milestone(m) 
-    }
-    person.save_changes
-    params.values.inspect
-  end
-
 end
---
+++ app/controllers/admin_doc.rb
@@ -0,0 +1,66 @@
+Alegato.controllers :doc_admin,  :parent => :doc do
+  layout :admin
+  get :index do
+    @doc = Document[params[:doc_id]]
+    @most_mentioned = @doc.person_dataset.order_by(:mentions).reverse.limit(10)
+    render "admin/doc/index"
+  end
+  post :reparse do
+    @doc = Document[params[:doc_id]]
+    @person_names = Hash.new{|hash,key| hash[key]=[]}
+    @doc.extract.person_names.each{|nombre| 
+      @person_names[Person.normalize_name(nombre)] << nombre 
+    }
+    params[:people].each{|n|
+      person_name = Person.normalize_name(n)
+      p = Person.find_or_create(:name => person_name)
+      @doc.add_person(p,@person_names[person_name].length) # link person and document and update the number of times this person gets mentioned
+    }
+  end
+  get :reparse do
+    @doc = Document[params[:doc_id]]
+    @person_names = Hash.new{|hash,key| hash[key]=[]}
+    @doc.extract.person_names.each{|nombre| 
+      @person_names[Person.normalize_name(nombre)] << nombre 
+    }
+    render "admin/doc/reparse"
+  end
+  get :persons do
+    @doc = Document[params[:doc_id]]
+    @people = @doc.person
+    render "admin/doc/persons"
+  end
+  get :person, :with => [:id] do
+    if params[:id].to_i == 0
+      @person = Person.filter_by_name(params[:id]).first
+    else
+      @person = Person[params[:id]]
+    end
+    @what = Milestone.what_list
+    @where = Milestone.where_list 
+    @doc = Document[params[:doc_id]]
+    person_name = ActiveSupport::Inflector.transliterate(@person.name).downcase
+    @fragments = @doc.extract.person_names.find_all{|name| 
+      name = ActiveSupport::Inflector.transliterate(name.to_s.downcase)
+      name == person_name
+    }
+    render "admin/doc/person"
+  end
+  post :person, :with => [:id] do
+    person = Person.where(:name => params[:name].strip).first || Person.create(:name => params[:name].strip)
+    Array(params[:person][:milestones]).each{|milestone|
+      data = milestone.dup
+      data.delete("id")
+      data[:what] = ! data["what_txt"].blank? ? data["what_txt"] : data["what_opc"]
+      data.delete("what_txt")
+      data.delete("what_opc")
+      data[:where] = ! data["where_txt"].blank? ? data["where_txt"] : data["where_opc"]
+      data.delete("where_txt")
+      data.delete("where_opc")
+      m=Milestone.new(data)
+      person.add_milestone(m) 
+    }
+    person.save_changes
+    params.values.inspect
+  end
+end
