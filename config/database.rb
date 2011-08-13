e1a47b20164805d297d6e42648e9aa36dbde45fd
Matías Iturburu <maturburu@gmail.com>
2011-08-13 14:03:07 -0300

    Agregamos un yaml para mantener las configuraciones locales afuera del repositorio.
    Para usar una configuración local deberías:
    
        $ cp config/local_config.yaml.example config/local_config.yaml
    
    editar la linea que dice:
        dsn: mysql2://user:pass@localhost/db_name
    
    para que se ajuste a tu configuración

--- config/database.rb
+++ config/database.rb
@@ -1,9 +1,5 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
-Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("mysql2://hhba:pepino@localhost/hhba", :loggers => [logger])
-  when :production  then Sequel.connect("mysql2://hhba:pepino@localhost/hhba",  :loggers => [logger])
-  when :test        then Sequel.connect("mysql2://hhba:pepino@localhost/hhba",  :loggers => [logger])
-end
+Sequel::Model.db = Sequel.connect(CONFIG['dsn'], :loggers => [logger])
 Sequel::Model.plugin :json_serializer
 Sequel::Model.plugin :timestamps
