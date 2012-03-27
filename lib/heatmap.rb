a646f4188261fc4ffd483c96b5c28022e905da3c
Damián Silvani <munshkr@gmail.com>
2012-03-27 18:18:42 -0300

    Tidy up lib

--- lib/heatmap.rb
+++ lib/heatmap.rb
@@ -1,57 +1,66 @@
 class Heatmap
   class Chunk < Array
     attr_accessor :idx
-    def initialize(chunk_idx,chunk_size)
+
+    def initialize(chunk_idx, chunk_size)
       @idx = chunk_idx
       @size = chunk_size
       super([])
     end
+
     def next_idx
       @idx + 1
     end
+
     def prev_idx
       @idx - 1
     end
+
     def abs_start_pos
       @idx * @size
     end
+
     def abs_end_pos
       abs_start_pos + @size
     end
   end
-  def initialize(size,chunk_size=1000,interpolate=true)
+
+  def initialize(size, chunk_size=1000, interpolate=true)
     @size = size
     @chunk_size = chunk_size.to_i
-    @chunks = Hash.new{|hash,k| hash[k]=Chunk.new(k,chunk_size.to_i)}
+    @chunks = Hash.new { |hash, k| hash[k] = Chunk.new(k, chunk_size.to_i) }
     @interpolate = interpolate
   end
-  def add_entry(entry_pos,entry)
-    chunk = Chunk.new((entry_pos.to_i / @chunk_size).to_i,@chunk_size)
+
+  def add_entry(entry_pos, entry)
+    chunk = Chunk.new((entry_pos.to_i / @chunk_size).to_i, @chunk_size)
     @chunks[chunk.prev_idx] << entry if @interpolate && chunk.idx > 0
     @chunks[chunk.idx] << entry
     @chunks[chunk.next_idx] << entry if @interpolate
   end
+
   def hot_chunks(n=10)
-    @chunks.sort_by{|chunk_id,values| values.length}.reverse[0..n]
+    @chunks.sort_by { |chunk_id, values| values.length }.reverse[0..n]
   end
-  def hot_areas(n=10, max_chunks_in_area = 10)
+
+  def hot_areas(n=10, max_chunks_in_area=10)
     areas = []
     in_area = []
-    @chunks.each{|idx,chunk|
+    @chunks.each do |idx, chunk|
       in_area << chunk
       # we'll add the current "area" to the "areas" array if the chunk with the next idx is empty
-      # or if the number of chunks in the area is < max_chunks_in_area 
+      # or if the number of chunks in the area is < max_chunks_in_area
       if not @chunks.has_key?(chunk.next_idx) or @chunks[chunk.next_idx].empty? or in_area.length >= max_chunks_in_area
         areas << in_area if in_area
         in_area = []
       end
-    }
-    areas.map{|chunks|
-      first = chunks.min_by{|chunk| chunk.idx}      
-      last = chunks.max_by{|chunk| chunk.idx}      
-      {:start => first.abs_start_pos, :end => last.abs_end_pos, :entries => chunks.flatten}
-    }.sort_by{|area|
-      area[:entries].length
-    }.reverse[0..n]
+    end
+    areas.map do |chunks|
+      first = chunks.min_by { |chunk| chunk.idx }
+      last = chunks.max_by { |chunk| chunk.idx }
+      { :start => first.abs_start_pos,
+        :end => last.abs_end_pos,
+        :entries => chunks.flatten }
+    end.sort_by { |area| area[:entries].length }.reverse[0..n]
   end
 end
--- lib/incomplete_date.rb
+++ lib/incomplete_date.rb
@@ -1,30 +1,34 @@
-require "date"
+require 'date'
+
 class IncompleteDate
   include Comparable
-  def initialize(year,month=nil,day=nil)
+
+  def initialize(year, month=nil, day=nil)
     if year.is_a?(Date)
       day = year.day
       month = year.month
       year = year.year
     end
     #convert falses in nils
-    @parts = [year || nil,month || nil,day || nil]
-    raise ArgumentError if ! year
+    @parts = [year || nil, month || nil, day || nil]
+    raise ArgumentError if not year
     from = Date.new(year, month || 1, day || 1)
     to   = Date.new(year, month || -1, day || -1)
     @range = (from .. to)
   end
+
   def self.parse(str)
     "Parses a string like '1999-00' into IncompleteDate.new(1999)"
     return nil if str.nil?
-    p = str.split(/[-\/]/).map{|n| n.to_i > 0 && n.to_i }
+    p = str.split(/[-\/]/).map { |n| n.to_i > 0 && n.to_i }
     return nil if p.length == 0
     new(*p)
   end
+
   def <=>(o)
     comparable_begin = o.respond_to?(:begin) ? o.begin : o
-    comparable_last = o.respond_to?(:last) ? o.last : o
-    if @range.begin < comparable_begin 
+    comparable_last  = o.respond_to?(:last)  ? o.last  : o
+    if @range.begin < comparable_begin
       -1
     elsif @range.end > comparable_last
       1
@@ -34,16 +38,20 @@ class IncompleteDate
       raise "I don't know how #{o} compares to #{self}"
     end
   end
+
   def to_s(fill=nil)
-    #fill the unknown parts with #{fill}
-    @parts.map{|n| n || fill}.compact.join("-")    
+    # fill the unknown parts with #{fill}
+    @parts.map { |n| n || fill }.compact.join("-")
   end
+
   def begin
     @range.begin
   end
+
   def last
     @range.last
   end
+
   def partial?
     "returns true if date is partial"
     @range.begin != @range.last
