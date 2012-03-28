1b70b5772e889319a0059ed244e31f9f6d08f798
Damián Silvani <munshkr@gmail.com>
2012-03-27 21:29:14 -0300

    Tidy up controllers

--- app/controllers/documents.rb
+++ app/controllers/documents.rb
@@ -1,11 +1,12 @@
-require "json"
-require "docsplit"
-require "open-uri"
-require "tempfile"
+require 'json'
+require 'docsplit'
+require 'open-uri'
+require 'tempfile'
 
 Alegato.controllers :documents do
   get :index do
     @docs = Document.all
+
     render "documents/index"
   end
 
@@ -15,12 +16,14 @@ Alegato.controllers :documents do
 
   get :search, :map => '/documents/search' do
     @docs = Document.where(:title => params[:title])
+
     render "documents/index"
   end
 
   get :show, :map => '/documents/:id' do
     @doc = Document.find(params[:id])
     @most_mentioned = []
+
     render "documents/show"
   end
 
@@ -28,21 +31,21 @@ Alegato.controllers :documents do
     data = params[:file][:tempfile].read
     text = Docsplit.clean_text(Docsplit.extract_text_from_pdf_str(data))
     title = Docsplit.extract_title_from_pdf_str(data)
-    @doc = Document.new :heading => params[:heading],
+    @doc = Document.new(:heading => params[:heading],
                         :description => params[:description],
-                        :category => params[:category]
+                        :category => params[:category])
     @doc.title = title
-    begin
-      @doc.published_at = Date.parse(params[:published_at]) unless params[:published_at].blank?
-    end
+    @doc.published_at = Date.parse(params[:published_at]) unless params[:published_at].blank?
     @doc.original_file = store_file(params[:file], data)
     @doc.data = text
+
     redirect url(:documents, :show, :id => @doc.id)
   end
 
   get :people, :map => '/documents/:id/people' do
     @doc = Document.find(params[:id])
     @people = @doc.people
+
     render "documents/people"
   end
 
@@ -56,60 +59,63 @@ Alegato.controllers :documents do
     @where = Milestone.where_list
     @doc = Document.find(params[:id])
     person_name = ActiveSupport::Inflector.transliterate(@person.name).downcase
-    @fragments = @doc.extract.person_names.find_all{|name|
+    @fragments = @doc.extract.person_names.find_all do |name|
       name = ActiveSupport::Inflector.transliterate(name.to_s.downcase)
       name == person_name
-    }
+    end
+
     render "documents/person"
   end
 
   post :person, :map => '/documents/:id/people/:person_id' do
     person = Person.find(params[:person_id])
     puts params[:person][:milestones].inspect
-    Array(params[:person][:milestones]).each{|idx,milestone|
+    Array(params[:person][:milestones]).each do |idx, milestone|
       data = milestone.dup
-      data[:what] = ! data["what_txt"].blank? ? data["what_txt"] : data["what_opc"]
+      data[:what] = !data["what_txt"].blank? ? data["what_txt"] : data["what_opc"]
       data.delete("what_txt")
       data.delete("what_opc")
-      data[:where] = ! data["where_txt"].blank? ? data["where_txt"] : data["where_opc"]
+      data[:where] = !data["where_txt"].blank? ? data["where_txt"] : data["where_opc"]
       data.delete("where_txt")
       data.delete("where_opc")
-      #convert dates in spanish into YY-MM-DD
+      # convert dates in spanish into YY-MM-DD
       data["date_from"] = data["date_from"].split(/[-\/]/).reverse.join("-")
       data["date_to"] = data["date_to"].split(/[-\/]/).reverse.join("-")
       if data[:person_id].to_i > 0
         id = data.delete("id")
-        m=Milestone.find(id)
+        m = Milestone.find(id)
         m.update_attributes(data)
       else
         data.delete("id")
-        m=Milestone.new(data)
+        m = Milestone.new(data)
       end
       person.milestones << m
-    }
+    end
     person.to_json
   end
 
   get :reparse, :map => '/documents/:id/reparse' do
     @doc = Document.find(params[:id])
-    @person_names = Hash.new{|hash,key| hash[key]=[]}
+    @person_names = Hash.new { |hash, key| hash[key] = [] }
     @doc.extract.person_names.each do |name|
       @person_names[Person.normalize_name(name)] << name
     end
+
     render "documents/reparse"
   end
 
   post :reparse, :map => '/documents/:id/reparse' do
     @doc = Document.find(params[:id])
-    @person_names = Hash.new{|hash,key| hash[key]=[]}
-    @doc.extract.person_names.each{|nombre|
-      @person_names[Person.normalize_name(nombre)] << nombre
-    }
-    params[:people].each{|n|
+    @person_names = Hash.new { |hash, key| hash[key] = [] }
+    @doc.extract.person_names.each do |name|
+      @person_names[Person.normalize_name(name)] << name
+    end
+    params[:people].each do |n|
       person_name = Person.normalize_name(n)
       p = Person.find_or_create_by(name: n)
       @doc.people << p
-    }
+    end
+
     redirect url_for(:documents, :show, :id => @doc._id)
   end
 
@@ -117,8 +123,9 @@ Alegato.controllers :documents do
     @doc = Document.find(params[:id])
     @heatmap_people = Heatmap.new(@doc.length)
     @heatmap_dates = Heatmap.new(@doc.length)
-    @doc.extract.person_names.each{|nombre| @heatmap_people.add_entry(nombre.start_pos,nombre) }
-    @doc.extract.dates.each{|date| @heatmap_dates.add_entry(date.start_pos,date) }
+    @doc.extract.person_names.each { |name| @heatmap_people.add_entry(name.start_pos, name) }
+    @doc.extract.dates.each { |date| @heatmap_dates.add_entry(date.start_pos, date) }
+
     render "documents/hot_zones"
   end
 
@@ -126,45 +133,48 @@ Alegato.controllers :documents do
     @doc = Document.find(params[:id])
     params[:start] = 0 if params[:start].to_i < 0
     @fragment = @doc.fragment(params[:start].to_i, params[:end].to_i)
+
     render "documents/curate"
   end
 
   get :context do
     if params[:fragment_id]
-      data=params[:fragment_id].match(/frag:doc=([^:]+):([0-9]+)-([0-9]+)/)
+      data = params[:fragment_id].match(/frag:doc=([^:]+):([0-9]+)-([0-9]+)/)
       puts params
-      doc_id=data[1]
-      pos_start=data[2]
-      pos_end=data[3]
+      doc_id = data[1]
+      pos_start = data[2]
+      pos_end = data[3]
     else
-      doc_id=params[:doc_id]
-      pos_start=params[:start]
-      pos_end=params[:end]
+      doc_id = params[:doc_id]
+      pos_start = params[:start]
+      pos_end = params[:end]
     end
 
     params[:around] ||= 1000
 
     case params[:action].to_i
-      when 1 # more
-          pos_start   = pos_start.to_i - params[:around].to_i
-          pos_start   = 0 if pos_start < 0
-          pos_end     = pos_end.to_i + params[:around].to_i
-      when 2 # less
-          pos_start   = pos_start.to_i + params[:around].to_i
-          pos_start   = 0 if pos_start < 0
-          pos_end     = pos_end.to_i - params[:around].to_i
-      when 3 # down
-          pos_start   = pos_start.to_i + params[:around].to_i
-          pos_end     = pos_end.to_i + params[:around].to_i
-      when 4 # up
-          pos_start   = pos_start.to_i - params[:around].to_i
-          pos_start   = 0 if pos_start < 0
-          pos_end     = pos_end.to_i - params[:around].to_i
+    when 1 # more
+      pos_start = pos_start.to_i - params[:around].to_i
+      pos_start = 0 if pos_start < 0
+      pos_end   = pos_end.to_i + params[:around].to_i
+    when 2 # less
+      pos_start = pos_start.to_i + params[:around].to_i
+      pos_start = 0 if pos_start < 0
+      pos_end   = pos_end.to_i - params[:around].to_i
+    when 3 # down
+      pos_start = pos_start.to_i + params[:around].to_i
+      pos_end   = pos_end.to_i + params[:around].to_i
+    when 4 # up
+      pos_start = pos_start.to_i - params[:around].to_i
+      pos_start = 0 if pos_start < 0
+      pos_end   = pos_end.to_i - params[:around].to_i
     end
 
-    fragment=Document.find(doc_id).fragment(pos_start,pos_end)
-    r={:fragment_id => fragment.fragment_id, :text => markup_fragment(fragment), :prev_fragment_id => params[:fragment_id]}.to_json
-    r
+    fragment = Document.find(doc_id).fragment(pos_start, pos_end)
+    r = {
+      :fragment_id => fragment.fragment_id,
+      :text => markup_fragment(fragment),
+      :prev_fragment_id => params[:fragment_id]
+    }.to_json
   end
-
 end
--- app/controllers/milestones.rb
+++ app/controllers/milestones.rb
@@ -1,9 +1,7 @@
 Alegato.controllers :milestones do
-
   get :index, :map => '/milestones' do
   end
 
   get :show, :map => '/milestones/:id' do
   end
-
 end
