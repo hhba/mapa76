a8ccc093052ae46d9b85a1fdb24ee656bd4886ba
Damián Silvani <munshkr@gmail.com>
2012-03-28 22:23:41 -0300

    Fix path issue when opening test data files

---
+++ test/test_helpers.rb
@@ -0,0 +1,5 @@
+require 'test/unit'
+
+def data_path(filename)
+  File.expand_path(File.join(File.dirname(__FILE__), "data", filename))
+end
--- test/test_import_pdf.rb
+++ test/test_import_pdf.rb
@@ -1,28 +1,27 @@
-#encoding: utf-8
-require "classifier"
-require File.join(File.expand_path(File.dirname(__FILE__)),"/../lib/text")
-require File.join(File.expand_path(File.dirname(__FILE__)),"/../lib/docsplit")
-require "test/unit"
+# encoding: utf-8
+require 'test_helpers'
+require 'docsplit'
+require 'text'
  
 class TestImportPdf < Test::Unit::TestCase
   def test_import
-    text = Docsplit.extract_text_from_pdf_str(open("data/f.pdf").read)
+    text = Docsplit.extract_text_from_pdf_str(open(data_path("f.pdf")).read)
     assert(text.length > 0)
-    assert_equal("Poder Judicial de la Nación\nAño del Bicentenario\nExpte. n° 563/99\n",Docsplit.find_header(text))
+    assert_equal("Poder Judicial de la Nación\nAño del Bicentenario\nExpte. n° 563/99\n", Docsplit.find_header(text))
     cleaned_text = Docsplit.clean_text(text)
     assert(cleaned_text.index(" fs. 32 y 72 y\nvta., Copello "), "Page number")
     assert_equal("///ta, 21 de diciembre de 2.010.
 AUTOS Y VISTOS:
 Para dictar sentencia en esta ca", cleaned_text[0..80])
   end
+
   def test_import2
-    text = Docsplit.extract_text_from_pdf_str(open("data/2.pdf").read)
+    text = Docsplit.extract_text_from_pdf_str(open(data_path("2.pdf")).read)
     assert(text.length > 0)
-    assert_equal("Poder Judicial de la Nación\nAño del Bicentenario\n",Docsplit.find_header(text))
+    assert_equal("Poder Judicial de la Nación\nAño del Bicentenario\n", Docsplit.find_header(text))
     cleaned_text = Docsplit.clean_text(text)
-    open("/tmp/w","w"){|fd| fd.write(cleaned_text)}
+    open("/tmp/w","w") { |fd| fd.write(cleaned_text) }
     assert_equal("\n///Plata, noviembre\n\nde 2010.-\n\nY VISTOS:\nEn el día de la fecha se reúnen los in", cleaned_text[0..80])
     assert(cleaned_text.index("a cargo de los\nseñores Fiscales"), "Page number")
   end
-
 end
