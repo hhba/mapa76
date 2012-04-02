284e8b00bdabb8a394f8500b22019533ef8fd1f0
Damián Silvani <munshkr@gmail.com>
2012-04-01 23:05:28 -0300

    remove deprecated code in Splitter

--- lib/splitter.rb
+++ lib/splitter.rb
@@ -46,25 +46,4 @@ module Splitter
     end
     nil
   end
-
-=begin
-  def self.extract_text_from_pdf_string(str)
-    tmp_file = Tempfile.new(["docsplit", ".pdf"], :encoding => "binary")
-    tmp_file.write(str)
-    tmp_file.close
-    basename = File.basename(tmp_file.path, "pdf")
-    tmp_dir = Dir.tmpdir
-    Docsplit.extract_text(tmp_file.path, :output => tmp_dir, :ocr => false)
-    open(File.join(tmp_dir, basename + "txt")).read
-  end
-
-  def self.extract_title_from_pdf_string(str)
-    tmp_file = Tempfile.new(["docsplit", ".pdf"], :encoding => "binary")
-    tmp_file.write(str)
-    tmp_file.close
-    basename = File.basename(tmp_file.path, "pdf")
-    tmp_dir = Dir.tmpdir
-    Docsplit.extract_title(tmp_file.path)
-  end
-=end
 end
