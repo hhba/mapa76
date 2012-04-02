e85552be7641a9dc0c37cdab09799c515c75e535
Damián Silvani <munshkr@gmail.com>
2012-04-01 23:37:20 -0300

    delete old deprecated files in config/ and tools/

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,7 @@ require 'yaml'
 # Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
-#CONFIG = YAML.load_file(::File.dirname(__FILE__)+'/local_config.yaml')[PADRINO_ENV]
+
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
