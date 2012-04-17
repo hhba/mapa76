ec45c2fc8fdf8d9460af68f0313dd8058e5aca25
Damián Silvani <munshkr@gmail.com>
2012-04-16 23:11:02 -0300

    Use Mongo :where for named entities helper methods

--- app/models/document.rb
+++ app/models/document.rb
@@ -96,15 +96,15 @@ class Document
   end
 
   def people_found
-    self.named_entities.select { |ne| ne.ne_class == :people }
+    self.named_entities.where(:ne_class => :people)
   end
 
   def dates_found
-    self.named_entities.select { |ne| ne.ne_class == :dates }
+    self.named_entities.where(:ne_class => :dates)
   end
 
   def organizations_found
-    self.named_entities.select { |ne| ne.ne_class == :organizations }
+    self.named_entities.where(:ne_class => :organizations)
   end
 
   def store_names
