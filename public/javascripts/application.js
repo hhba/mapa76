81df395a6af659d5842a5ba0114e62d67d7ee619
David Lima Cohen <david@limacohen.com>
2012-05-05 16:54:05 -0300

    Drag and drop

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -44,6 +44,10 @@ $(document).ready(function(){
         "json"
       );
   });
+	$('.paragraphs span.ne').draggable({
+		helper: 'clone'
+	}).css('cursor', 'move');
+	init_drops();
        // $.post("<%=url_for :admin_classify_name %>",
        //          {name: $(e.currentTarget).parents("li").children("span.name").text(), training: $(e.currentTarget).attr("value")},
        //          function(b){$(e.currentTarget).parents("span.buttons").text("res")},
@@ -51,3 +55,16 @@ $(document).ready(function(){
        //        )
        //  }
 });
+function init_drops(){
+	$('.event.new').removeClass('new').droppable({
+        drop: function(ev, ui) {
+        	if ($(this).html() == '')
+        	{
+        		$(this).removeClass('empty').after('<div ondragover="event.preventDefault()" class="event new empty"></div>');
+        		init_drops();
+        	}
+        	  var draggable = ui.draggable;
+        	$(this).append('<span class="dragged ' + draggable.attr('class') + '">' + draggable.text() + '<i></i></span><wbr>');
+        }
+     });
+}
