28e93be007d3ef5996d24b881818cb199c306fa0
Damián Silvani <munshkr@gmail.com>
2012-05-12 15:10:43 -0300

    Configure Resque connection options with a YML file

---
+++ config/resque.rb
@@ -0,0 +1,9 @@
+require 'resque'
+
+# Taken from config/boot.rb
+PADRINO_ENV  = ENV['PADRINO_ENV'] ||= ENV['RACK_ENV'] ||= 'development'  unless defined?(PADRINO_ENV)
+PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
+
+# Configure Resque
+resque_config = YAML.load_file(File.join(PADRINO_ROOT, 'config', 'resque.yml'))
+Resque.redis  = resque_config[PADRINO_ENV]
