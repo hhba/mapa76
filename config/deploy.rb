c5bf90aeb0e4df0f64a34d1eb4a628bb64e02484
Damián Silvani <munshkr@gmail.com>
2012-06-10 17:59:17 -0300

    Add new Cap tasks to remotely manage Monit and workers

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
-require 'bundler/capistrano'
-require 'rvm/capistrano'
+require "bundler/capistrano"
+require "rvm/capistrano"
 
 set :application, "mapa76"
 
 set :user, "mapa"
 set :domain, "hhba.info"
+set :workers, "ec2-23-22-65-238.compute-1.amazonaws.com"
 set :environment, "production"
 set :deploy_to, "/home/mapa/#{application}"
 
-role :app, domain
+role :app, domain, workers
 role :web, domain
 role :db, domain, :primary => true
 
@@ -25,17 +26,21 @@ set :ssh_options, :forward_agent => true
 
 set :keep_releases, 5
 
+set :config_files, %w{ mongoid resque monit workers }
+
+
 namespace :deploy do
   desc "symlink to mongoid.yml"
   task :create_symlink_shared do
-    run "ln -nfs #{deploy_to}/shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
-    run "ln -nfs #{deploy_to}/shared/config/resque.yml #{release_path}/config/resque.yml"
+    config_files.each do |filename|
+      run "ln -nfs #{deploy_to}/shared/config/#{filename}.yml #{release_path}/config/#{filename}.yml"
+    end
     run "ln -nfs #{deploy_to}/shared/uploads #{release_path}/public/uploads"
     run "ln -nfs #{deploy_to}/shared/thumbs #{release_path}/public/thumbs"
   end
 
   task :migrate do
-    puts "no mgirations"
+    puts "no migrations"
   end
 
   desc "restarting"
@@ -44,4 +49,49 @@ namespace :deploy do
   end
 end
 
-after 'deploy:update_code', 'deploy:create_symlink_shared'
+namespace :monit do
+  desc "Start Monit daemon (rebuilding config file)"
+  task :start, :roles => :app do
+    rake "monit:start"
+  end
+
+  desc "Stop Monit daemon"
+  task :stop, :roles => :app do
+    rake "monit:stop"
+  end
+
+  desc "Restart Monit daemon (rebuilding config file)"
+  task :restart, :roles => :app do
+    rake "monit:restart"
+  end
+
+  desc "Generate *only* Monit configuration file"
+  task :config, :roles => :app do
+    rake "monit:config"
+  end
+end
+
+namespace :workers do
+  desc "Gracefully reload workers (waits for jobs to finish, then restarts processes)"
+  task :reload, :roles => :workers do
+    rake "workers:reload"
+  end
+
+  desc "Immediately kill workers, cancelling any current job and re-enqueuing them"
+  task :stop, :roles => :workers do
+    rake "workers:stop"
+  end
+
+  desc "Pause all workers (stop processing new jobs)"
+  task :pause, :roles => :workers do
+    rake "workers:pause"
+  end
+
+  desc "Resume all workers (begin processing new jobs)"
+  task :resume, :roles => :workers do
+    rake "workers:resume"
+  end
+end
+
+after "deploy:update_code", "deploy:create_symlink_shared"
+after "deploy", "workers:reload"
