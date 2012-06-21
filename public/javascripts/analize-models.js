3f8a5e282971a472fa64294a83e8fa60650e4c08
Marcos Vanetta <marcos.vanetta@bvision.com>
2012-06-21 15:58:37 -0300

    split analize.js in different files

---
+++ public/javascripts/analize-models.js
@@ -0,0 +1,43 @@
+var Document = Backbone.Model.extend({
+  urlRoot: '/api/documents/'
+});
+var Person = Backbone.Model.extend({
+  urlRoot: "/api/people/"
+});
+var Paragraph = Backbone.Model.extend({});
+var Register = Backbone.Model.extend({
+  urlRoot: "/api/registers/",
+  loadValues: function(info) {
+    var that = this;
+    _.each(info, function(value, key){
+      that.set(key, value, {silent: true});
+    });
+    return that;
+  },
+  validate: function(attribs){
+    console.log(attribs);
+    if(attribs.who.length === 0) {
+      return "There must be a who";
+    } else if (attribs.when.length === 0) {
+      return "There must be a when";
+    } else if (attribs.where.length === 0) {
+      return "There must be a where";
+    } else if (attribs.what === null) {
+      return "There must be a what";
+    }
+  },
+  defaults: {
+    who    : [],
+    where  : [],
+    when   : [],
+    to_who : [],
+    what   : []
+  }
+});
+var ParagraphList = Backbone.Collection.extend({
+  model: Paragraph,
+  url: function(){
+    return '/api/documents/' + this.get("document_id");
+  }
+});
+
