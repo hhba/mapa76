6e09fa9dfdca21515a2cc24b8a750bc12f3ac20d
Damián Silvani <munshkr@gmail.com>
2012-07-15 15:19:53 -0300

    Tell pdfinfo to encode strings to UTF-8 when extracting metadata (#45)

--- lib/ext/info_extractor.rb
+++ lib/ext/info_extractor.rb
@@ -5,12 +5,8 @@ module Docsplit
     # Pull out a single datum from a pdf.
     def extract(key, pdfs, opts)
       pdf = [pdfs].flatten.first
-      cmd = "pdfinfo #{ESCAPE[pdf]} 2>&1"
+      cmd = "pdfinfo -enc UTF-8 #{ESCAPE[pdf]} 2>&1"
       result = `#{cmd}`.chomp
-
-      # FIX encoding issues
-      result.encode('UTF-8', :invalid => :replace, :replace => '').encode('UTF-8')
-
       raise ExtractionFailed, result if $? != 0 
       match = result.match(MATCHERS[key])
       answer = match && match[1]
