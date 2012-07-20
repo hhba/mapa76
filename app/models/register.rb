3166b5eaed45e8ee5a8da842f648492c46313b81
Damián Silvani <munshkr@gmail.com>
2012-07-20 00:53:00 -0300

    Store ObjectIds on FactRegister and RelationRegister models

--- app/models/register.rb
+++ app/models/register.rb
@@ -2,35 +2,58 @@ class Register
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :who,    type: Array
-  field :when,   type: Array
-  field :where,  type: Array
-  field :to_who, type: Array
-  field :what,   type: String
+  #field :who,    type: Array
+  #field :when,   type: Array
+  #field :where,  type: Array
+  #field :to_who, type: Array
+  #field :what,   type: String
+  #
+  #belongs_to :document, index: true
+  #
+  #def self.actions
+  #  ActionEntity::VERBS
+  #end
+  #
+  #def self.build_and_save(values)
+  #  whats = values.delete("what")
+  #  whats.map do |what|
+  #    values["what"] = what
+  #    self.create(values)
+  #  end
+  #end
+  #
+  #def to_hash
+  #  {
+  #    who:    self.who.map { |w| NamedEntity.find(w).text },
+  #    what:   self.what,
+  #    when:   self.when.map { |w| NamedEntity.find(w).text },
+  #    where:  self.where.map { |w| NamedEntity.find(w).text },
+  #    to_who: self.to_who.map { |w| NamedEntity.find(w).text }
+  #  }
+  #end
 
-  belongs_to :document, index: true
+  # FIXME
+  #def self.timeline_json
+  #  all.map { |register| register.timeline_json }.reject {|r| !r}.to_json
+  #end
 
-  def self.actions
-    ActionEntity::VERBS
-  end
+  # FIXME
+  #def timeline_json
+  #  unless self.when.empty? or who.empty? or where.empty?
+  #    { order: NamedEntity.find(self.when.first).timestamp, character: NamedEntity.find(who.first).text, group: NamedEntity.find(where.first).text }
+  #  end
+  #end
 
-  def self.timeline_json
-    all.map { |register| register.timeline_json }.reject {|r| !r}.to_json
-  end
+protected
+  def self.references(fields, opts={})
+    raise ArgumentError, ":type option is missing" if not opts[:type]
+    fields = Array(fields)
+    klass = opts[:type]
 
-  def to_hash
-    {
-      who:    self.who.map { |w| NamedEntity.find(w).text },
-      what:   self.what,
-      when:   self.when.map { |w| NamedEntity.find(w).text },
-      where:  self.where.map { |w| NamedEntity.find(w).text },
-      to_who: self.to_who.map { |w| NamedEntity.find(w).text }
-    }
-  end
-
-  def timeline_json
-    unless self.when.empty? or who.empty? or where.empty?
-      { order: NamedEntity.find(self.when.first).timestamp, character: NamedEntity.find(who.first).text, group: NamedEntity.find(where.first).text }
+    fields.each do |field|
+      define_method(field) do
+        self.send("#{field}_id") and klass.find(self.send("#{field}_id"))
+      end
     end
   end
 end
