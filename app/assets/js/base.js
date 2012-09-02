a363241e26b0f094138927faa8b7215b03a71d93
Damián Silvani <munshkr@gmail.com>
2012-09-02 18:34:28 -0300

    Move Bootstrap to vendor/assets/bootstrap

--- app/assets/js/base.js
+++ app/assets/js/base.js
@@ -1,18 +1,15 @@
 //= require "javascripts/jquery-1.7.2.min"
+
+//= require "bootstrap/js/bootstrap"
+
 //= require "javascripts/jquery-ui-1.8.22.custom.min"
 //= require "javascripts/jq_spin_plugin"
 //= require "javascripts/underscore-min"
 //= require "javascripts/backbone-min"
 //= require "javascripts/mustache"
 //= require "javascripts/spin.min"
-//= require "javascripts/bootstrap.min"
-//= require "javascripts/bootstrap-button"
-//= require "javascripts/bootstrap-alert"
-//= require "javascripts/bootstrap-collapse"
-//= require "javascripts/bootstrap-tab"
-//= require "javascripts/bootstrap-tooltip"
-//= require "javascripts/bootstrap-popover"
 //= require "javascripts/jquery.tablesorter.min"
 //= require "javascripts/jquery.mousewheel.min"
 //= require "javascripts/jquery.mCustomScrollbar"
+
 //= require "js/application"
