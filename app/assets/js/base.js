86f6f8325bcff439564bd98682161aa5fe848cef
Damián Silvani <munshkr@gmail.com>
2012-09-02 19:28:44 -0300

    Move Mustache.js to vendor/assets/js

--- app/assets/js/base.js
+++ app/assets/js/base.js
@@ -4,10 +4,10 @@
 
 //= require "js/underscore"
 //= require "js/backbone"
-//
+//= require "js/mustache"
+
 //= require "javascripts/jquery-ui-1.8.22.custom.min"
 //= require "javascripts/jq_spin_plugin"
-//= require "javascripts/mustache"
 //= require "javascripts/spin.min"
 //= require "javascripts/jquery.tablesorter.min"
 //= require "javascripts/jquery.mousewheel.min"
