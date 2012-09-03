e70c5d7d95f172fccb2fe6f5adea1910479abfb0
Damián Silvani <munshkr@gmail.com>
2012-09-03 11:46:53 -0300

    Move the rest of the JS files to vendor/assets

--- app/assets/js/base.js
+++ app/assets/js/base.js
@@ -6,11 +6,11 @@
 //= require "js/backbone"
 //= require "js/mustache"
 
-//= require "javascripts/jquery-ui-1.8.22.custom.min"
-//= require "javascripts/jq_spin_plugin"
-//= require "javascripts/spin.min"
-//= require "javascripts/jquery.tablesorter.min"
-//= require "javascripts/jquery.mousewheel.min"
-//= require "javascripts/jquery.mCustomScrollbar"
+//= require "js/jquery-ui-1.8.22.custom.min"
+//= require "js/jq_spin_plugin"
+//= require "js/spin.min"
+//= require "js/jquery.tablesorter.min"
+//= require "js/jquery.mousewheel.min"
+//= require "js/jquery.mCustomScrollbar"
 
 //= require "js/application"
