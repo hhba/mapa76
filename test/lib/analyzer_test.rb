fe7a437d56a9ec34dcc9ef1c9cb8864af15fd365
Damián Silvani <munshkr@gmail.com>
2012-11-18 14:29:03 -0300

    analyzer: Update tests

--- test/lib/analyzer_test.rb
+++ test/lib/analyzer_test.rb
@@ -23,10 +23,12 @@ class TestAnalyzer < Test::Unit::TestCase
       "actualmente con prisión domiciliaria."
   end
 
-  def test_extract_tokens
+  def test_extract_tokens_empty_string
     tokens = Analyzer.extract_tokens("")
     assert_equal [], tokens.to_a
+  end
 
+  def test_extract_tokens_short_text
     tokens = Analyzer.extract_tokens(@short)
     assert_equal [
       {:form=>"Alegato", :pos=>0},
@@ -43,49 +45,49 @@ class TestAnalyzer < Test::Unit::TestCase
       {:form=>"Diciembre", :pos=>43},
       {:form=>"2010", :pos=>53},
       {:form=>".", :pos=>57}
-    ], tokens.to_a
+    ].map(&:stringify_keys), tokens.to_a
   end
 
   def test_extract_named_entities
     named_entities = Analyzer.extract_named_entities(@text)
 
-    person = named_entities.find{|ne| ne[:form] == "Luis_Abelardo_Patti"}
+    person = named_entities.find { |ne| ne.form == "Luis_Abelardo_Patti" }
     assert person
-    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:people], person[:tag]
-    assert_equal 0, person[:pos]
-    assert_equal 0, person[:sentence_pos]
+    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:people], person.tag
+    assert_equal 0, person.pos
+    assert_equal 0, person.sentence_pos
     assert_equal [
-        {:pos=>0, :form=>"Luis"},
-        {:pos=>5, :form=>"Abelardo"},
+        {:pos=>0,  :form=>"Luis"},
+        {:pos=>5,  :form=>"Abelardo"},
         {:pos=>14, :form=>"Patti"}
-      ], person[:tokens]
+      ].map(&:stringify_keys), person.tokens
 
-    date = named_entities.find{|ne| ne[:form] == "día_26_de_noviembre_de_1952"}
+    date = named_entities.find { |ne| ne.form == "día_26_de_noviembre_de_1952" }
     assert date
-    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:dates], date[:tag]
-    assert_equal 45, date[:pos]
-    assert_equal 0, date[:sentence_pos]
-    assert_equal "[??:26/11/1952:??.??:??]", date[:lemma]
+    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:dates], date.tag
+    assert_equal 45, date.pos
+    assert_equal 0, date.sentence_pos
+    assert_equal "[??:26/11/1952:??.??:??]", date.lemma
   end
 
   def test_extract_named_entities_multiple_sentences
     named_entities = Analyzer.extract_named_entities(@two_sentences)
 
-    person_a = named_entities.find{|ne| ne[:form] == "Reynaldo_Benito_Antonio_Bignone"}
+    person_a = named_entities.find{|ne| ne.form == "Reynaldo_Benito_Antonio_Bignone"}
     assert person_a
-    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:people], person_a[:tag]
-    assert_equal 0, person_a[:sentence_pos]
+    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:people], person_a.tag
+    assert_equal 0, person_a.sentence_pos
 
-    person_b = named_entities.find{|ne| ne[:form] == "Juan_Fernando_Meneghini"}
+    person_b = named_entities.find{|ne| ne.form == "Juan_Fernando_Meneghini"}
     assert person_b
-    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:people], person_b[:tag]
-    assert_equal 1, person_b[:sentence_pos]
+    assert_equal NamedEntity::CLASSES_PER_TAG.invert[:people], person_b.tag
+    assert_equal 1, person_b.sentence_pos
   end
 
   def test_extract_addresses
     named_entities = Analyzer.extract_named_entities("Scalabrini Ortiz 1234, Sanchez de Bustamante al 2000.").to_a
 
-    address_a = named_entities.find { |ne| ne[:tag] == NamedEntity::CLASSES_PER_TAG.invert[:addresses] }
+    address_a = named_entities.find { |ne| ne.tag == NamedEntity::CLASSES_PER_TAG.invert[:addresses] }
     assert address_a
   end
 end
