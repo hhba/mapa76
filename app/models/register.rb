42eab385831f0874661caa21ad58890061fcfc86
Damián Silvani <munshkr@gmail.com>
2012-11-19 14:51:25 -0300

    Move Register.references to a Mongoid extension

--- app/models/register.rb
+++ app/models/register.rb
@@ -1,36 +1,9 @@
 class Register
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::References
 
   def self.actions
     ActionEntity::VERBS
-  end
-
-  # FIXME
-  #def self.build_and_save(values)
-  #  whats = values.delete("what")
-  #  whats.map do |what|
-  #    values["what"] = what
-  #    self.create(values)
-  #  end
-  #end
-
-protected
-  def self.references(fields, opts={})
-    raise ArgumentError, ":type option is missing" if not opts[:type]
-    fields = Array(fields)
-    klass = opts[:type]
-
-    fields.each do |field|
-      if field_singular = field.to_s.singularize.to_sym and field == field_singular
-        define_method(field) do
-          self.send("#{field}_id") and klass.find(self.send("#{field}_id"))
-        end
-      else
-        define_method(field) do
-          klass.where.in(_id: (self.send("#{field_singular}_ids") || [])).to_a
-        end
-      end
-    end
   end
 end
