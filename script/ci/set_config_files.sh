3a16a00c0e76fc090a6eb436af1d577f06f36527
Damián Silvani <munshkr@gmail.com>
2013-03-12 20:39:07 -0300

    TravisCI: Copy elasticsearch.yml file before running

--- script/ci/set_config_files.sh
+++ script/ci/set_config_files.sh
@@ -2,3 +2,4 @@
 cp -v config/resque.yml.sample config/resque.yml
 cp -v config/monit.yml.sample config/monit.yml
 cp -v config/workers.yml.sample config/workers.yml
+cp -v config/elasticsearch.yml.sample config/elasticsearch.yml
