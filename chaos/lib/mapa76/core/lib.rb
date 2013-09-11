e89713a3c737b7e3a7fb13ad7e275ffc98a25ecc
malev <marcosvanetta@gmail.com>
2013-09-11 01:38:34 -0300

    [chaos] Remove glue_task

--- chaos/lib/mapa76/core/lib.rb
+++ chaos/lib/mapa76/core/lib.rb
@@ -1,7 +1,6 @@
 autoload :Finder, "mapa76/core/lib/finder"
 autoload :TimeSetter, "mapa76/core/lib/time_setter"
 autoload :DocumentProcessBootstrapTask, "mapa76/core/lib/document_process_bootstrap_task"
-autoload :GlueTask, "mapa76/core/lib/glue_task"
 
 require "mongoid"
 require "resque"
--- chaos/lib/mapa76/core/lib/glue_task.rb
+++
@@ -1,9 +0,0 @@
-class GlueTask
-  @queue = :misc
-
-  # NOTE Analyzers should override this method
-  # and reenqueue Document to the first step of the process.
-  def self.perform(document_id)
-    raise NotImplementedError
-  end
-end
