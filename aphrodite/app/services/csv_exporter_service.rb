749d3c5df2856a8754b5512161f81bfd67aeddc8
malev <marcosvanetta@gmail.com>
2013-09-18 14:51:22 -0300

    [aphrodite] Add new fields to CSV #126

--- aphrodite/app/services/csv_exporter_service.rb
+++ aphrodite/app/services/csv_exporter_service.rb
@@ -10,30 +10,38 @@ class CSVExporterService
 
   def export_people
     export(:people_found,
-           %w{ _id document_id text lemma tag prob pos sentence_pos page_num })
+           %w{ document_id text lemma tag prob pos sentence_pos page_num })
   end
 
   def export_dates
     export(:dates_found,
-           %w{ _id document_id text lemma tag prob pos sentence_pos page_num time })
+           %w{ document_id text lemma tag prob pos sentence_pos page_num time })
   end
 
   def export_places
     export([:places_found, :addresses_found],
-           %w{ _id document_id text lemma tag prob pos sentence_pos page_num lat lng })
+           %w{ document_id text lemma tag prob pos sentence_pos page_num lat lng })
   end
 
   def export_organizations
     export(:organizations_found,
-           %w{ _id document_id text lemma tag prob pos sentence_pos page_num })
+           %w{ document_id text lemma tag prob pos sentence_pos page_num })
   end
 
   def original_filename
-    document.original_filename
+    @original_filename ||= document.original_filename
+  end
+
+  def title
+    @title ||= document.title
+  end
+
+  def date
+    @date ||= document.created_at.strftime("%m/%d/%Y - %H:%M")
   end
 
   def link_to_doc
-    "#{@hostname}documents/#{@document._id}"
+    "#{@hostname}documents/#{@document._id}/comb"
   end
 
 private
@@ -41,11 +49,15 @@ private
   def export(finders, keys)
     finders = Array(finders)
     CSV.generate do |csv|
-      csv << keys + ['link_to_doc']
+      csv << keys + ['Title', 'Filename', 'Date', 'link_to_doc']
       finders.each do |finder|
         document.public_send(finder).each do |ne|
           row = keys.map { |k| ne.respond_to?(k) ? ne.public_send(k) : nil }
+          row << title
+          row << original_filename
+          row << date
           row << link_to_doc
+
           csv << row
         end
       end
