0daf46a3d0fad10392f1cb28c0ec76ff5b753efc
malev <marcosvanetta@gmail.com>
2013-09-23 22:44:23 -0300

    [hephaestus] Add progress bar class

---
+++ hephaestus/lib/progress_bar.rb
@@ -0,0 +1,40 @@
+class ProgressBar
+  attr_accessor :starting_at, :ending_at, :current, :bound
+
+  def initialize
+    @current = 0
+    @starting_at = 0
+    @ending_at = 100
+    @bound = 100
+  end
+
+  def increment
+    i = 1 * (ending_at - starting_at).to_f / bound
+    @current = @current + i
+  end
+
+  def progress
+    @current > ending_at ? ending_at : @current
+  end
+
+  def progress=(value)
+    @current = value
+  end
+
+  def starting_at=(value)
+    @starting_at = value
+    @current = value
+  end
+
+  def ending_at=(value)
+    @ending_at = value
+  end
+
+  def bound=(value)
+    @bound = value
+  end
+
+  def inspect
+    "#<ProgressBar: #{current}/#{bound}>"
+  end
+end
---
+++ hephaestus/test/lib/progress_bar_test.rb
@@ -0,0 +1,68 @@
+require 'test_helper'
+
+describe ProgressBar do
+  context 'normal conditions' do
+    describe '#increment and #progress' do
+      it 'increments %' do
+        progress_bar = ProgressBar.new
+        progress_bar.increment
+        progress_bar.progress.must_equal 1
+        progress_bar.increment
+        progress_bar.progress.must_equal 2
+      end
+    end
+
+    describe '#progress=' do
+      it 'changes progress' do
+        progress_bar = ProgressBar.new
+        progress_bar.progress = 10
+        progress_bar.progress.must_equal 10
+      end
+    end
+  end
+
+  describe '#starting_at=' do
+    it 'changes the starting point' do
+      progress_bar = ProgressBar.new
+      progress_bar.starting_at = 10
+      progress_bar.progress = 10
+      progress_bar.increment
+      progress_bar.progress = 11
+    end
+  end
+
+  describe '#ending_at' do
+    it 'never pass ending_at point' do
+      progress_bar = ProgressBar.new
+      progress_bar.ending_at = 60
+      progress_bar.bound = 3
+      progress_bar.increment
+      progress_bar.increment
+      progress_bar.increment
+      progress_bar.increment
+      progress_bar.progress.must_equal 60
+    end
+  end
+
+  context "change unit's scale" do
+    describe '#bound=' do
+      it 'changes the bound' do
+        progress_bar = ProgressBar.new
+        progress_bar.bound = 10
+        progress_bar.progress.must_equal 0
+        progress_bar.increment
+        progress_bar.progress.must_equal 10
+      end
+
+      it 'changes scale and bound' do
+        progress_bar = ProgressBar.new
+        progress_bar.bound = 300
+        progress_bar.starting_at = 20
+        progress_bar.ending_at = 50
+        progress_bar.progress.must_equal 20
+        10.times { progress_bar.increment }
+        progress_bar.progress.must_be_within_delta 21
+      end
+    end
+  end
+end
