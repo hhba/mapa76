5730c5c4f7e7e745602037bd4d9048076b24522d
malev <marcosvanetta@gmail.com>
2013-09-24 00:58:02 -0300

    [hephaestus] Fix 80 chars line

--- hephaestus/lib/resque/document_process_bootstrap_task.rb
+++ hephaestus/lib/resque/document_process_bootstrap_task.rb
@@ -6,7 +6,9 @@ class DocumentProcessBootstrapTask
     task_finder = TaskFinder.new(document)
 
     unless task_finder.last_task? and task_finder.current_task_ended?
-      klass = Kernel.const_get(task_finder.next_task.split('_').map(&:capitalize).join)
+      klass = Kernel.const_get(
+        task_finder.next_task.split('_').map(&:capitalize).join
+      )
       Resque.enqueue(klass, document_id)
     end
   end
