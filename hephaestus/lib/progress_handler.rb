5832422c91142d2615e6f5c3a29231a35b40470f
malev <marcosvanetta@gmail.com>
2013-09-24 00:55:48 -0300

    [hephaestus] Add ProgressHandler class

---
+++ hephaestus/lib/progress_handler.rb
@@ -0,0 +1,38 @@
+class ProgressHandler
+  attr_accessor :document, :bound
+
+  def initialize(document, opt={})
+    @document = document
+    @bound = opt.fetch(:bound, 100)
+  end
+
+  def increment
+    progress_bar.increment
+    update(progress_bar.progress)
+  end
+
+  def progress_bar
+    @progress_bar ||= ProgressBar.new starting_at: starting_at,
+      ending_at: ending_at, bound: bound
+  end
+
+  def starting_at
+    task_finder.index * step
+  end
+
+  def ending_at
+    (task_finder.index + 1) * step
+  end
+
+  def step
+    100.0 / task_finder.count
+  end
+
+  def task_finder
+    @task_finder ||= TaskFinder.new(document)
+  end
+
+  def update(value)
+    document.update_attribute :percentage, value
+  end
+end
---
+++ hephaestus/test/lib/progress_handler_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+describe ProgressHandler do
+  context 'document has not been analized' do
+    let(:document) { stub(status: '', tasks: []) }
+
+    before do
+      TaskFinder.any_instance.stubs(:tasks_count).returns(4)
+      @progress_handler = ProgressHandler.new(document)
+    end
+
+    describe '#starting_at' do
+      it 'starts on 0' do
+        @progress_handler.starting_at.must_equal 0
+      end
+    end
+
+    describe '#ending_at' do
+      it 'ends on 25' do
+        @progress_handler.ending_at.must_equal 25
+      end
+    end
+
+    describe '#increment' do
+      it 'updates the document status' do
+        document.expects(:update_attribute).with(:percentage, 0.25)
+        @progress_handler.increment
+      end
+    end
+  end
+
+  context 'document has one task completed' do
+    let(:document) { stub(status: '', tasks: []) }
+
+    before do
+      TaskFinder.any_instance.stubs(:count).returns(4)
+      TaskFinder.any_instance.stubs(:index).returns(1)
+      @progress_handler = ProgressHandler.new(document)
+    end
+
+    describe '#starting_at' do
+      it 'starts on 25' do
+        @progress_handler.starting_at.must_equal 25
+      end
+    end
+
+    describe '#ending_at' do
+      it 'ends on 50' do
+        @progress_handler.ending_at.must_equal 50
+      end
+    end
+
+    describe '#increment' do
+      it 'updates the document status' do
+        document.expects(:update_attribute).with(:percentage, 25.25)
+        @progress_handler.increment
+      end
+    end
+  end
+end
