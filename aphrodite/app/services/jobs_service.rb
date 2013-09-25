201d706ad4899817e554d8297eead3a838f05751
malev <marcosvanetta@gmail.com>
2013-09-25 16:51:36 -0300

    [aphrodite] Add JobsSerive

---
+++ aphrodite/app/services/jobs_service.rb
@@ -0,0 +1,26 @@
+class JobsSerivice
+  def self.working_on?(document)
+    new(document).working?
+  end
+
+  attr_reader :document
+
+  def initialize(document)
+    @document = document
+  end
+
+  def working?
+    get_working_ids.include?(document.id)
+  end
+
+  def get_working_ids
+    Resque.working.map do |job|
+      begin
+        job["payload"]["args"].first
+      rescue NoMethodError
+        nil
+      end
+    end.flatten
+  end
+
+end
---
+++ aphrodite/test/services/jobs_service_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+describe JobsSerivice do
+  let(:jobs) do
+    {
+      "queue"=>"extraction_task",
+      "run_at"=>"2013-09-25T18:06:58Z",
+      "payload"=>{
+        "class"=>"ExtractionTask",
+        "args"=>["524326399f9d515a54000009"]
+      }
+    }
+  end
+
+  describe '#working_on?' do
+    context 'there is no job working' do
+      it 'returns false' do
+        Resque.stubs(working: [])
+        document = FactoryGirl.create :document
+
+        JobsSerivice.working_on?(document).must_equal false
+      end
+    end
+
+    context 'there is a job working on a different document' do
+      it 'returns false' do
+        Resque.stubs(working: [jobs])
+        document = FactoryGirl.create :document
+
+        JobsSerivice.working_on?(document).must_equal false
+      end
+    end
+
+    context 'job working on current document' do
+      it 'returns true' do
+        Resque.stubs(working: [jobs])
+        document = stub(id: "524326399f9d515a54000009")
+
+        JobsSerivice.working_on?(document).must_equal true
+      end
+    end
+  end
+end
