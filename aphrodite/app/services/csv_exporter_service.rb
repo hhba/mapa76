9f1c71332eb5e0aefe3eda486ce486e11ab15e69
malev <marcosvanetta@gmail.com>
2013-10-02 18:02:43 -0300

    [aphrodite] Organization CSV includes mentions

--- aphrodite/app/services/csv_exporter_service.rb
+++ aphrodite/app/services/csv_exporter_service.rb
@@ -10,9 +10,9 @@ class CSVExporterService
 
   def export_people
     CSV.generate do |csv|
-      csv << ['person_id', 'Name', 'Mentions', 'Title', 'Filename', 'Date', 'DocumentID', 'link_to_doc']
+      csv << %w[PersonID Name Mentions].concat(document_info_str)
       document.people.each do |person|
-        csv << [person.id, person.name, mentions(person), title, date, document.id, link_to_doc]
+        csv << [person.id, person.name, mentions(person), title, original_filename, date, document.id, link_to_doc]
       end
     end
   end
@@ -32,8 +32,14 @@ class CSVExporterService
   end
 
   def export_organizations
-    export(:organizations_found,
-           %w{ document_id text lemma tag prob pos sentence_pos page_num })
+    organization_groups = document.organizations_found.group_by(&:text)
+    CSV.generate do |csv|
+      csv << %w(Name Mentions).concat(document_info_str)
+      organization_groups.each do |group|
+        org = group.first
+        csv << [org, group.count].concat(document_info)
+      end
+    end
   end
 
   def original_filename
@@ -50,6 +56,14 @@ class CSVExporterService
 
   def link_to_doc
     "#{@hostname}documents/#{@document._id}/comb"
+  end
+
+  def document_info
+    [document.title, original_filename, date, link_to_doc]
+  end
+
+  def document_info_str
+    %w[Title Filename Date DocumentId link_to_doc]
   end
 
 private
--- aphrodite/test/services/csv_exporter_service_test.rb
+++ aphrodite/test/services/csv_exporter_service_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 describe CSVExporterService do
-  it 'fails' do
-    assert false
+  let(:document) { FactoryGirl.create :document }
+  let(:exporter) { CSVExporterService.new(document) }
+
+  describe '#document_info' do
+    it '' do
+      exporter.stubs(date: '11/11/11', link_to_doc: '/')
+      exporter.document_info.must_equal [
+        document.title, document.original_filename, '11/11/11', '/'
+      ]
+    end
+  end
+
+  describe '#link_to_file' do
+    it '' do
+      link = "http://mapa76.info/documents/#{document.id}/comb"
+
+      exporter.link_to_doc.must_equal link
+    end
+  end
+
+  describe '#original_filename' do
+    it '' do
+      exporter.original_filename.must_equal document.original_filename
+    end
   end
 end
