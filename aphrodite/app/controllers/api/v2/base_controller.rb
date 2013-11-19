3288a4a0de32b38e53c67a3a092ba56b6023f4f6
malev <marcosvanetta@gmail.com>
2013-11-19 16:37:18 -0300

    [aphrodite] Add document-ids to allowed headers

--- aphrodite/app/controllers/api/v2/base_controller.rb
+++ aphrodite/app/controllers/api/v2/base_controller.rb
@@ -32,7 +32,7 @@ protected
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Expose-Headers'] = 'ETag'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Authorization'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Authorization,Document-Ids'
     headers['Access-Control-Max-Age'] = '86400'
   end
 
