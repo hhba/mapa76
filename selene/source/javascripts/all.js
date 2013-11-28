fba7a73e724cf8d45053b6b0730717fa18aa8fb6
malev <marcosvanetta@gmail.com>
2013-11-28 16:11:57 -0300

    [selene] Add mapa76.info html files

---
+++ selene/source/javascripts/all.js
@@ -0,0 +1,6 @@
+//= require jquery-1.10.2.min
+//= require jquery.placeholder
+//= require jquery.carouFredSel-6.2.1-packed
+//= require carouFredsSel_cfg
+//= require dropdown
+//= require jquery.icheck.min
---
+++ selene/source/javascripts/carouFredsSel_cfg.js
@@ -0,0 +1,45 @@
+
+//HOME CAROUSEL
+$(function() {
+	$('#content-caroufredsel').carouFredSel({
+			items				: 1,
+			author 				: false,
+			height				: 377,
+			width				: 567,
+			pagination 			: "#content-caroufredsel-pager",
+			mousewheel 			: true,
+			auto 	 			: true,
+			circular 			: true,
+			infinite 			: true,
+
+			scroll: {
+				items			: 1,
+				duration		: 1000,							
+				pauseOnHover	: true
+			}					
+		});
+	});
+
+
+//HOME PARTNERS
+$(function() {
+	$('#prefooter-caroufredsel').carouFredSel({
+			items				: 6,
+			author 				: false,
+			height				: 63,
+			width				: 845,
+			circular 			: true,
+			prev 				: '#prev2',
+			next 				: '#next2',
+			mousewheel 			: true,
+			auto 	 			: false,
+			infinite 			: true,
+
+			scroll: {
+				items			: 1,
+				duration		: 1000,							
+				pauseOnHover	: true
+			}					
+		});
+	});
+
