c90182d1ac1f8111e7b5c0757f8ed6284f08b5e0
malev <marcosvanetta@gmail.com>
2013-12-02 13:31:45 -0300

    [hephaestus] Add places, address and orgs to mentions finder

--- hephaestus/lib/resque/mentions_finder_task.rb
+++ hephaestus/lib/resque/mentions_finder_task.rb
@@ -1,6 +1,10 @@
+require 'active_support/all'
+
 class MentionsFinderTask < Base
   attr_accessor :document, :user, :named_entities
   @queue = :mentions_finder_task
+
+  NE_CLASSES = [:organizations, :addresses, :places]
 
   def self.perform(document_id)
     self.new(document_id).call
@@ -9,35 +13,46 @@ class MentionsFinderTask < Base
   def initialize(document_id)
     @document = Document.find(document_id)
     @user = @document.user
-    @named_entities = @document.named_entities.where(ne_class: 'organizations')
   end
 
   def call
-    dupplicates.each do |org|
-      organization = user.organizations.where(name: org[:name]).first
-      if organization
-        organization.mentions = organization.mentions.merge({document.id.to_s => org[:size]})
-        organization.save
-      else
-        organization = Organization.create name: org[:name],
-                                           mentions: { document.id.to_s => org[:size] }
-        user.organizations << organization
-      end
-      document.organizations << organization
+    NE_CLASSES.each do |ne_class|
+      find_and_store(ne_class)
     end
   end
 
-  def dupplicates
-    org_names = named_entities.map(&:text)
-    org_names.uniq.map do |org_name|
+  def find_and_store(ne_class)
+    klass = build_klass(ne_class)
+    dupplicates(ne_class).each do |dup|
+      entity = user.send(ne_class).where(name: dup[:name]).first
+      if entity
+        entity.mentions = entity.mentions.merge({document.id.to_s => dup[:size]})
+        entity.save
+      else
+        entity = klass.create name: dup[:name], mentions: { document.id.to_s => dup[:size]}
+        user.send(ne_class) << entity
+      end
+      document.send(ne_class) << entity
+    end
+  end
+
+  def dupplicates(ne_class)
+    named_entities = document.named_entities.where(ne_class: ne_class).to_a
+    entity_lemmas = named_entities.map(&:lemma)
+    entity_lemmas.uniq.map do |entity_lemma|
+      same_lemma_named_entities = named_entities.select { |ne| ne.lemma == entity_lemma }
       {
-        name: org_name,
-        size: named_entities.select { |ne| ne.text == org_name }.size
+        size: same_lemma_named_entities.count,
+        name: same_lemma_named_entities.first.form
       }
     end
   end
 
   def documents
     @documents ||= user.documents
+  end
+
+  def build_klass(ne_class)
+    ne_class.to_s.singularize.capitalize.constantize
   end
 end
