45cb5d62354858138ef5dc1e24bf60222f3f1beb
malev <marcosvanetta@gmail.com>
2013-12-06 18:52:46 -0300

    [aphrodite] Remove useless conditions

--- aphrodite/app/assets/javascripts/home.js
+++ aphrodite/app/assets/javascripts/home.js
@@ -1,42 +1,38 @@
 $(document).ready(function(){
-  if($('#content-caroufredsel').length) {
-    $('#content-caroufredsel').carouFredSel({
-        items       : 1,
-        author        : false,
-        height        : 377,
-        width       : 567,
-        pagination      : "#content-caroufredsel-pager",
-        mousewheel      : true,
-        auto        : true,
-        circular      : true,
-        infinite      : true,
+  $('#content-caroufredsel').carouFredSel({
+    items       : 1,
+    author        : false,
+    height        : 377,
+    width       : 567,
+    pagination      : "#content-caroufredsel-pager",
+    mousewheel      : true,
+    auto        : true,
+    circular      : true,
+    infinite      : true,
 
-        scroll: {
-          items     : 1,
-          duration    : 1000,
-          pauseOnHover  : true
-        }
-      });
-  }
+    scroll: {
+      items     : 1,
+      duration    : 1000,
+      pauseOnHover  : true
+    }
+  });
 
-  if($('#prefooter-caroufredsel').length) {
-    $('#prefooter-caroufredsel').carouFredSel({
-      items       : 6,
-      author        : false,
-      height        : 63,
-      width       : 845,
-      circular      : true,
-      prev        : '#prev2',
-      next        : '#next2',
-      mousewheel      : true,
-      auto        : false,
-      infinite      : true,
+  $('#prefooter-caroufredsel').carouFredSel({
+    items       : 6,
+    author        : false,
+    height        : 63,
+    width       : 845,
+    circular      : true,
+    prev        : '#prev2',
+    next        : '#next2',
+    mousewheel      : true,
+    auto        : false,
+    infinite      : true,
 
-      scroll: {
-        items     : 1,
-        duration    : 1000,
-        pauseOnHover  : true
-      }
-    });
-  }
+    scroll: {
+      items     : 1,
+      duration    : 1000,
+      pauseOnHover  : true
+    }
+  });
 });
