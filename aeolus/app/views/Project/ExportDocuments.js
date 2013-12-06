4cc92393137cef9b2ba15e7262e2e74fbce8e7d6
Pablo Novas <pjnovas@gmail.com>
2013-12-06 18:29:57 -0300

    [aeolus] added multiple export documents

---
+++ aeolus/app/views/Project/ExportDocuments.js
@@ -0,0 +1,74 @@
+/**
+ * VIEW: ExportDocuments
+ * Toolbar section for exporting by class the selected documents
+ */
+
+var 
+  template = require('./templates/exportDocuments.tpl');
+
+module.exports = Backbone.Marionette.ItemView.extend({
+
+  //--------------------------------------
+  //+ PUBLIC PROPERTIES / CONSTANTS
+  //--------------------------------------
+
+  template: template,
+
+  ui: {
+    classes: "input[type=radio]",
+    exportLink: "#do-export"
+  },
+
+  templateHelpers: function(){
+    return {
+      uploadURL: aeolus.rootURL + "/documents",
+      token: aeolus.authKey
+    };
+  },
+
+  modelEvents: {
+    "change:counter": "render"
+  },
+
+  //--------------------------------------
+  //+ INHERITED / OVERRIDES
+  //--------------------------------------
+
+  onDomRefresh: function(){
+    this.ui.classes.iCheck({
+        checkboxClass: 'icheckbox_flat-grey left',
+        radioClass: 'iradio_flat-grey left',
+        increaseArea: '20%'
+      })
+      .on('ifChecked', this.onCheck.bind(this))
+      .first().iCheck("check");
+  },
+
+  //--------------------------------------
+  //+ PUBLIC METHODS / GETTERS / SETTERS
+  //--------------------------------------
+
+  //--------------------------------------
+  //+ EVENT HANDLERS
+  //--------------------------------------
+  
+  onCheck: function(e){
+    this.updateLinkURL($(e.currentTarget).val());
+  },
+
+  //--------------------------------------
+  //+ PRIVATE AND PROTECTED METHODS
+  //--------------------------------------
+
+  updateLinkURL: function(type){
+    var url = aeolus.rootURL + "/documents/export?";
+
+    var ids = aeolus.app.project.get("documents").getSelectedIds();
+    url += "ids=" + ids.join(",");
+
+    url += "&class=" + type;
+
+    this.ui.exportLink.attr("href", url);
+  }
+
+});
