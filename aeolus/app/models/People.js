62272ef53574b3d46bccac2563c6c4c2d0a985b2
malev <marcosvanetta@gmail.com>
2013-12-18 14:48:42 -0300

    [aeolus][aphrodite] Add mentions and name sorting

--- aeolus/app/models/People.js
+++ aeolus/app/models/People.js
@@ -1,4 +1,3 @@
-
 var Person = require("./Person");
 
 module.exports = Backbone.Collection.extend({
@@ -6,7 +5,20 @@ module.exports = Backbone.Collection.extend({
   model: Person,
 
   url: function(){
-    return aeolus.rootURL + "/people"; 
+    return aeolus.rootURL + "/people";
+  },
+
+  changeSort: function(prop, order) {
+    order = typeof order !== 'undefined' ? order : 'asc';
+    order = order === "asc" ? -1 : 1;
+
+    this.comparator = function(a, b){
+      if(a.get(prop) > b.get(prop)) { return -order; }
+      if(a.get(prop) < b.get(prop)) { return order; }
+      return 0;
+    };
+
+    this.sort().trigger('reset');
   }
 
 });
