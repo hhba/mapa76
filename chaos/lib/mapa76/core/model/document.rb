729a63092770840dbf5f23b64fefed77c25ba003
malev <marcosvanetta@gmail.com>
2013-12-30 03:19:16 -0300

    [chaos] Change context generation

--- chaos/lib/mapa76/core/model/document.rb
+++ chaos/lib/mapa76/core/model/document.rb
@@ -158,21 +158,14 @@ protected
   end
 
   def context_generator
-    ctx = {
+    {
       :id => id,
       :title => title,
-      :registers => self.fact_registers.map(&:to_hash),
       :people => self.people.map { |person| { id: person.id, name: person.full_name, mentions: person.mentions_in(self) } },
-      :dates => self.dates_found.group_by(&:text).map { |k, v| { text: k, mentions: v.size} },
-      :organizations => self.organizations_found.group_by(&:text).map { |k, v| { text: k, mentions: v.size} },
-      :places => (self.places_found + self.addresses_found).group_by(&:text).map { |k, v| { text: k, mentions: v.size} }
+      :dates => self.date_entities.map { |date| {id: date.id, name: date.name, mentions: date.mentions[self.id.to_s]}},
+      :organizations => self.organizations.map { |org| {id: org.id, name: org.name, mentions: org.mentions[self.id.to_s]}},
+      :places => self.places.map { |place| {id: place.id, name: place.name, mentions: place.mentions[self.id.to_s]}}
     }
-
-    [:registers, :people, :dates, :organizations, :places].each do |f|
-      ctx[:"has_#{f}"] = !ctx[f].empty?
-    end
-
-    ctx
   end
 
   def set_default_title
