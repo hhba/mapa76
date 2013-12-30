34c2d0e2e2f4a3d988f17700c4c9b0f53c9ecb27
malev <marcosvanetta@gmail.com>
2013-12-30 03:19:03 -0300

    [aeolus] Add document-info view

---
+++ aeolus/app/views/Document/DocumentInfo.js
@@ -0,0 +1,11 @@
+/**
+ * VIEW: DocumentInfo
+ * 
+ */
+
+var 
+  template = require('./templates/documentInfo.tpl');
+
+module.exports = Backbone.Marionette.CompositeView.extend({
+  template: template
+});
