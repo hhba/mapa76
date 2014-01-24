a1a0a805b74bd94a5060b0b745e857a50ba31ede
malev <marcosvanetta@gmail.com>
2014-01-24 15:31:51 -0300

    [aphrodite] New search option WIP

--- aphrodite/app/services/searcher_service.rb
+++ aphrodite/app/services/searcher_service.rb
@@ -32,7 +32,51 @@ class SearcherService
     end
   end
 
+  def search(str)
+    user_id = user.id
+    documents_search = Tire.search(documents_index) do
+      query do
+        boolean do
+          must { string str }
+          must { term :user_id, user_id}
+        end
+      end
+    end
+
+    pages_search = Tire.search(pages_index) do
+      query do
+        boolean do
+          must { string str }
+          must { term :user_id, user_id}
+        end
+      end
+    end
+    
+    entities_search = Tire.search(entities_index) do
+      query do
+        boolean do
+          must { string str }
+          must { term :user_id, user_id}
+        end
+      end
+    end
+    
+    [documents_search, pages_search, entities_search]
+  end
+
   def store(query)
     Search.create user: user, term: query
+  end
+
+  def documents_index
+    "documents_#{Rails.env}"
+  end
+
+  def pages_index
+    "pages_#{Rails.env}"
+  end
+
+  def entities_index
+    "entities_#{Rails.env}"
   end
 end
