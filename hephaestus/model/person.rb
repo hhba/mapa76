7534da2c6b6c7327474fd91d661d0e89cf23b7d2
malev <marcosvanetta@gmail.com>
2014-01-24 23:56:55 -0300

    [hephaestus] Remove unused code

--- hephaestus/model/person.rb
+++ hephaestus/model/person.rb
@@ -5,34 +5,6 @@ class Person
     Person.all.select { |person| person.tags.include?("conadep")}
   end
 
-  def self.populate(document, duplicates, opts={})
-    # This is a really ugly code, but it works, I'm not gonna change it now!
-    tags = opts.fetch :tags, []
-    confidence = opts[:confidence] ? opts[:confidence] : 0.0
-
-    output = []
-    duplicates.each do |group|
-      already_added = group.map do |ne|
-        where(user_id: document.user_id).searchable_with(ne.text).first
-      end.compact
-
-      if already_added.empty?
-        person = Person.create(:name => group.first.text,
-                               :tags => tags,
-                               :confidence => confidence,
-                               :user_id => document.user_id,
-                              )
-      else
-        person = already_added.first
-      end
-      person.named_entities.concat(group)
-      person.documents << document
-      person.save!
-      output << person
-    end
-    output
-  end
-
   def self.filter_by_name(name, is_prefix=false)
     #name +="%" if is_prefix
     #self.filter(:searchable_name.like(normalize_name(name)))
