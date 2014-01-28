d0fa3f1ee4b012399388423958a718105b21d7d7
malev <marcosvanetta@gmail.com>
2014-01-27 22:40:33 -0300

    [aphrodite] New searcher service

--- aphrodite/app/services/searcher_service.rb
+++ aphrodite/app/services/searcher_service.rb
@@ -1,39 +1,16 @@
+require 'ostruct'
+
 class SearcherService
+  include ActionView::Helpers::TextHelper
+
   attr_accessor :user, :q, :ids
 
   def initialize(user)
     @user = user
   end
 
-  def where(opt={})
-    @q = opt[:q]
-    @ids = opt.fetch(:ids, [])
-    store(@q) unless @q.nil?
-    call
-  end
-
-  def call
-    this = self
-    results = Document.tire.search do
-      query do
-        boolean do
-          must { string this.q }
-          must { term :user_id, this.user.id }
-          must { ids values: this.ids } unless this.ids.empty?
-        end
-      end
-      highlight :title, *(1..10000).map(&:to_s)
-      facet "users" do
-        terms :user_id
-      end
-    end
-    results.map do |item|
-      [item, item.load]
-    end
-  end
-
-  def search(str)
-    output = []
+  def where(str)
+    store(str)
     user_id = user.id
     documents_search = Tire.search(documents_index) do
       query do
@@ -52,22 +29,31 @@ class SearcherService
         end
       end
     end
-    
-    # entities_search = Tire.search(entities_index) do
-    #   query do
-    #     boolean do
-    #       must { string str }
-    #       must { term :user_id, user_id}
-    #     end
-    #   end
-    # end
 
-    [documents_search, pages_search].each do |search|
-      search.results.each do |result|
-        output << result
+    output = []
+    documents_search.results.each do |result|
+      output << result
+    end
+
+    pages_search.results.group_by(&:document_id).each do |document_id, pages|
+      begin
+        document = Document.find(document_id)
+        highlights = {}
+        pages.group_by(&:num).each do |num, page_groups|
+          text_groups = page_groups.map(&:text)
+          highlights[num] = {text: "<p>" + text_groups.join("</p><p>") + "</p>"}
+        end
+        output << OpenStruct.new(title: document.title,
+                                 document_id: document.id,
+                                 original_filename: document.original_filename,
+                                 created_at: document.created_at,
+                                 highlight: highlights)
+      rescue Mongoid::Errors::DocumentNotFound
+        nil
       end
     end
-    output.sort(&:_score)
+
+    output
   end
 
   def store(query)
