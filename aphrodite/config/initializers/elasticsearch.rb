5ff97807960205372c2e846c160e44e9c745f687
malev <marcosvanetta@gmail.com>
2014-02-20 21:57:08 -0300

    [aphrodite] Add elasticsearch api config options

---
+++ aphrodite/config/initializers/elasticsearch.rb
@@ -0,0 +1,10 @@
+require 'elasticsearch'
+
+module Mapa76
+  class Application < Rails::Application
+    options = YAML.load_file(File.join(Rails.root, "config", "elasticsearch.yml"))[Rails.env]
+    logger = Logger.new(File.expand_path(options["log"], Rails.root))
+    host = options["url"]
+    config.elasticsearch_client = Elasticsearch::Client.new host: host, logger: logger
+  end
+end
