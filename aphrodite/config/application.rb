04569dcfcd5951bb0b62f9fa730ce2d01b476638
malev <marcosvanetta@gmail.com>
2014-02-20 21:57:08 -0300

    [aphrodite] Add elasticsearch api config options

--- aphrodite/config/application.rb
+++ aphrodite/config/application.rb
@@ -72,6 +72,7 @@ module Mapa76
 
     # Thumbnails and upload paths
     config.thumbnails_path = "/thumbs"
+    config.elasticsearch_client = ""
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
       "#{html_tag}".html_safe
