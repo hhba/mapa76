c20e6434bbe59627e174af3b52fd7b7cc7af8279
malev <marcosvanetta@gmail.com>
2014-02-23 00:04:58 -0300

    [app] Change elasticsearch index_prefix

--- aphrodite/config/initializers/elasticsearch.rb
+++ aphrodite/config/initializers/elasticsearch.rb
@@ -6,5 +6,6 @@ module Mapa76
     logger = Logger.new(File.expand_path(options["log"], Rails.root))
     host = options["url"]
     config.elasticsearch_client = Elasticsearch::Client.new host: host, logger: logger
+    config.elasticsearch_prefix = options["index_prefix"]
   end
 end
