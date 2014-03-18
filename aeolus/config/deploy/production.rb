5a283bb8009aaaa76d7c3fdc65286cd98fb76b19
malev <marcosvanetta@gmail.com>
2014-03-18 18:08:55 -0300

    [aeolus] Add capistrano deploy files

---
+++ aeolus/config/deploy/production.rb
@@ -0,0 +1,2 @@
+server "184.173.160.186", :web, :app, :workers, :db, primary: true
+set :grunt_command, "prod"
---
+++ aeolus/config/deploy/staging.rb
@@ -0,0 +1,2 @@
+server "162.243.4.112", :web, :app, :workers, :db, primary: true
+set :grunt_command, 'stage'
