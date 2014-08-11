35089d22aaff1467258084560be3a3dc58875831
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-10 19:15:29 -0500

    Fix missing autoload

--- chaos/lib/mapa76/core/lib.rb
+++ chaos/lib/mapa76/core/lib.rb
@@ -1,6 +1,7 @@
 autoload :Finder, "mapa76/core/lib/finder"
 autoload :TimeSetter, "mapa76/core/lib/time_setter"
 autoload :DocumentProcessBootstrapTask, "mapa76/core/lib/document_process_bootstrap_task"
+autoload :LinksProcessorTask, "mapa76/core/lib/links_processor_task"
 
 require "mongoid"
 require "resque"
