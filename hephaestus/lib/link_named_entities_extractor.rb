d9f9c9622d753b1acc0090a18b9cb0dbdfd7efe1
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-22 00:26:52 -0500

    [hephaestus] NamedEntities storer for links

--- hephaestus/lib/link_named_entities_extractor.rb
+++ hephaestus/lib/link_named_entities_extractor.rb
@@ -6,10 +6,51 @@ class LinkNamedEntitiesExtractor
   def call
     entites = @document.people + @document.organizations + @document.places
     @document.pages.each do |page|
-      find_entities_in(page, entities)
+      entites.each do |entity|
+        find_all(page, entity.name).each do |position|
+          store(entity, position)
+        end
+      end
     end
   end
 
-  def find_entities_in(page, entities)
+  def store(entity, position)
+    NamedEntity.create({
+      text: entity.name,
+      pos: position[:from_pos],
+      inner_pos: position,
+      entity_id: entity.id,
+      ne_class: entity_class(entity),
+      document: @document
+    })
+  end
+
+  def entity_class(entity)
+    case entity.class
+    when Person
+      :people
+    when Organization
+      :organizations
+    when Place
+      :places
+    else
+      nil
+    end
+  end
+
+  def find_all(page, substring)
+    index = 0
+    output = []
+
+    while (pos = page.text.index(substring, index)) do
+      output << {
+        from_pos: pos,
+        to_pos: pos + substring.length,
+        pid: page.id.to_s
+      }
+
+      index = pos + substring.length
+    end
+    output
   end
 end
