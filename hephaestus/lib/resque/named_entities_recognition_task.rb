d06ca889cf37a267ce91d61bae1a308d1ffed1f4
Marcos Vanetta <marcosvanetta@gmail.com>
2014-12-15 16:42:52 -0600

    [hephaestus] Named entities recognition task

---
+++ hephaestus/lib/resque/named_entities_recognition_task.rb
@@ -0,0 +1,25 @@
+class NamedEntitiesRecognitionTask < BaseTask
+  @queue = :named_entities_recognition_task
+  @msg = 'Named entities recognition'
+  @next_task = 'store_named_entities_task'
+
+  attr_reader :text, :metadata
+
+  def initialize(input)
+    @text = input.fetch('data', '')
+    @metadata = input.fetch('metadata', {})
+  end
+
+  def call
+    analyzer_client = AnalyzerClient.new(text)
+
+    tokens = analyzer_client.tokens.to_a
+
+    puts tokens.to_json
+
+    @output = {
+      'data' => tokens,
+      'metadata' => @metadata
+    }
+  end
+end
---
+++ hephaestus/test/lib/resque/named_entities_recognition_task_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+describe NamedEntitiesRecognitionTask do
+  let(:output) {
+    output = NamedEntitiesRecognitionTask.new({
+      'data' => 'string',
+      'metadata' => {}
+    }).call
+  }
+
+  before do
+    analyzer = Object.new
+    analyzer.expects(:tokens).returns({})
+    AnalyzerClient.expects(:new).with('string').returns(analyzer)
+  end
+
+  it 'returns tokens' do
+    output['data'].must_equal []
+  end
+end
