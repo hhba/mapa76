efff9545a5f58a3c29a5171e115bec725ba1b593
Marcos Vanetta <marcosvanetta@gmail.com>
2014-12-19 14:40:45 -0600

    [aphrodite] Remove user's validations

--- aphrodite/app/models/user.rb
+++ aphrodite/app/models/user.rb
@@ -10,7 +10,6 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validate :has_invitation_token, unless: "invitation_token.nil?"
   validate :username, presence: true
 
   before_create :generate_access_token
@@ -21,12 +20,6 @@ class User
   end
 
 private
-
-  def has_invitation_token
-     unless Invitation.burn!(invitation_token)
-       errors.add(:invitation_token)
-     end
-  end
 
   def generate_access_token
     self.access_token = SecureRandom.hex
