f26283a799bf44ed194bdbd8f30634ca7d63da2c
Marcos Vanetta <marcosvanetta@gmail.com>
2014-12-19 14:38:33 -0600

    [aphrodite] remove invitation's methods

--- aphrodite/test/models/invitation_test.rb
+++ aphrodite/test/models/invitation_test.rb
@@ -1,30 +1,6 @@
 require 'test_helper'
 
 describe Invitation do
-  context 'class methods' do
-    describe '#burn!' do
-      context 'invitation not burned' do
-        it 'returns true' do
-          invitation = FactoryGirl.create :invitation
-          Invitation.burn!(invitation.token).must_equal true
-        end
-      end
-
-      context 'invitation already burned' do
-        it 'returns false' do
-          invitation = FactoryGirl.create :invitation, burned_at: Time.now
-          Invitation.burn!(invitation.token).must_equal false
-        end
-      end
-
-      context 'invitation not exist' do
-        it 'returns false' do
-          Invitation.burn!('notoken').must_equal false
-        end
-      end
-    end
-  end
-
   describe 'validations' do
     it '' do
       invitation = Invitation.new
@@ -41,31 +17,6 @@ describe Invitation do
       invitation.token.must_equal ''
       invitation.save
       invitation.token.wont_equal ''
-    end
-  end
-
-  describe '#burn!' do
-    it '' do
-      invitation = FactoryGirl.create :invitation
-      invitation.burned_at.must_equal nil
-      invitation.burn!
-      invitation.burned_at.must_be_instance_of DateTime
-    end
-  end
-
-  describe '#burned?' do
-    context 'already burned' do
-      it '' do
-        invitation = FactoryGirl.create :invitation,  burned_at: Time.now
-        invitation.burned?.must_equal true
-      end
-    end
-
-    context 'not burned' do
-      it '' do
-        invitation = FactoryGirl.create :invitation
-        invitation.burned?.must_equal false
-      end
     end
   end
 end
