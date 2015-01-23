489d2f39c28bac7eeeb4e968a6c4027def2f1eeb
Marcos Vanetta <marcosvanetta@gmail.com>
2015-01-22 19:26:41 -0600

    [aphrodite] elasticsearch.yml handle ERB

--- aphrodite/config/initializers/elasticsearch.rb
+++ aphrodite/config/initializers/elasticsearch.rb
@@ -2,7 +2,9 @@ require 'elasticsearch'
 
 module Mapa76
   class Application < Rails::Application
-    options = YAML.load_file(File.join(Rails.root, "config", "elasticsearch.yml"))[Rails.env]
+    options = YAML.load(
+      ERB.new(File.read("#{Rails.root}/config/elasticsearch.yml")).result
+    )[Rails.env]
     logger = Logger.new(File.expand_path(options["log"], Rails.root))
     host = options["url"]
     config.elasticsearch_client = Elasticsearch::Client.new host: host, logger: logger
