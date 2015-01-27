82700e992b6289ea924b8b48f90fa3c846f3ae70
Marcos Vanetta <marcosvanetta@gmail.com>
2015-01-26 20:34:29 -0600

    [aphrodite] Fix resque initializer

--- aphrodite/config/initializers/resque.rb
+++ aphrodite/config/initializers/resque.rb
@@ -1,5 +1,7 @@
 rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
 rails_env = Rails.env || 'development'
 
-resque_config = YAML.load_file(rails_root.to_s + '/config/resque.yml')
+resque_config = YAML.load(
+  ERB.new(File.read(Rails.root.join('config', 'resque.yml'))).result
+)
 Resque.redis = resque_config[rails_env]
