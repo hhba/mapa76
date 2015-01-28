f29b4f8b884e43c62696938f4b9868625d678b08
Pablo Novas <pjnovas@gmail.com>
2015-01-28 15:38:35 -0300

    updated MOCK server API

--- aeolus/mockup-server/routes.js
+++ aeolus/mockup-server/routes.js
@@ -5,31 +5,31 @@ module.exports = function(){
     res.send('Mockup Server');
   });
 
-  app.get('/api/v1/documents', function(req, res){
+  app.get('/api/v2/documents', function(req, res){
     res.send(require('./data/documents'));
   });
 
-  app.del('/api/v1/documents', function(req, res){
+  app.del('/api/v2/documents', function(req, res){
     res.send(204);
   });
 
-  app.get('/api/v1/documents/search', function(req, res){
+  app.get('/api/v2/documents/search', function(req, res){
     res.send(require('./data/documents_search'));
   });
 
-  app.del('/api/v1/documents/:id', function(req, res){
+  app.del('/api/v2/documents/:id', function(req, res){
     res.send(204);
   });
 
-  app.get('/api/v1/documents/status', function(req, res){
+  app.get('/api/v2/documents/status', function(req, res){
     res.send(require('./data/status'));
   });
 
-  app.get('/api/v1/people', function(req, res){
+  app.get('/api/v2/people', function(req, res){
     res.send(require('./data/people'));
   });
 
-  app.get('/api/v1/people/:id', function(req, res){
+  app.get('/api/v2/people/:id', function(req, res){
     res.send(require('./data/person'));
   });
 
