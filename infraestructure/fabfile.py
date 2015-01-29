0c4c7dab434e61c729e8d4574571fb63251d61e2
Marcos Vanetta <marcosvanetta@gmail.com>
2015-01-28 21:15:34 -0600

    [infra] Deploy 5 unicorn workers

--- infraestructure/fabfile.py
+++ infraestructure/fabfile.py
@@ -30,9 +30,13 @@ def deploy_aphrodite():
         sudo('docker rm -f aphrodite-1')
         sudo('docker rm -f aphrodite-2')
         sudo('docker rm -f aphrodite-3')
+        sudo('docker rm -f aphrodite-4')
+        sudo('docker rm -f aphrodite-5')
     sudo('docker run -d -p 3001:8080 --env-file=config.dat --name=aphrodite-1 -v ~/log:/app/aphrodite/log malev/aphrodite')
     sudo('docker run -d -p 3002:8080 --env-file=config.dat --name=aphrodite-2 -v ~/log:/app/aphrodite/log malev/aphrodite')
     sudo('docker run -d -p 3003:8080 --env-file=config.dat --name=aphrodite-3 -v ~/log:/app/aphrodite/log malev/aphrodite')
+    sudo('docker run -d -p 3004:8080 --env-file=config.dat --name=aphrodite-4 -v ~/log:/app/aphrodite/log malev/aphrodite')
+    sudo('docker run -d -p 3005:8080 --env-file=config.dat --name=aphrodite-5 -v ~/log:/app/aphrodite/log malev/aphrodite')
 
 def deploy_schedulers():
     put('config.dat', '~/')
