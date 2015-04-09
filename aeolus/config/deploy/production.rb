776d024a38dca890b296938db7d0993d2cced221
malev <marcosvanetta@gmail.com>
2015-04-08 22:27:48 -0500

    [aeolus] Remove capistrano from aeolus

--- aeolus/config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-server "191.237.72.87", :web, :app, :workers, :db, primary: true
-set :grunt_command, "prod"
-
-set :ssh_options, {
-  auth_methods: ["publickey"],
-  keys: ["~/.ssh/myPrivateKey.key"]
-}
--- aeolus/config/deploy/staging.rb
+++
@@ -1,3 +0,0 @@
-server "107.170.13.39", :web, :app, :workers, :db, primary: true
-set :grunt_command, 'prod'
-set :user, "deploy"
