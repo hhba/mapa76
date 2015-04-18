0eeb1604f2c6e7ebef2539a8433b493db12f595d
malev <marcosvanetta@gmail.com>
2015-04-18 14:06:49 -0500

    [infraestructure] Assets deploy

--- infraestructure/assets.rb
+++ infraestructure/assets.rb
@@ -14,9 +14,20 @@ Dir['../aeolus/dist/**/*'].reject {|fn| File.directory?(fn) }.each do |file|
   azure_blob_service.create_block_blob(
     'aeolus', # Container name
     blob,
-    File.open(file, "rb") { |file| file.read }
+    File.open(file, "rb") { |f| f.read }
   )
 end
 
+Dir['../aeolus/dist/**/*'].reject {|fn| File.directory?(fn) }.each do |file|
+  blob = "latest/#{file.split('/')[3..-1].join('/')}"
+  puts "Uploading #{blob}"
+  azure_blob_service.create_block_blob(
+    'aeolus', # Container name
+    blob,
+    File.open(file, "rb") { |f| f.read }
+  )
+end
+
+puts "Your base domain is http://az745887.vo.msecnd.net/aeolus/#{release_hash}"
+puts "Your base domain is http://az745887.vo.msecnd.net/aeolus/latest"
 puts "Release hash: #{release_hash}"
-__END__
